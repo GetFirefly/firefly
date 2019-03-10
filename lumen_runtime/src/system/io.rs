04c9d57ae351b8d456802c1ca9737640380d4371
Luke Imhoff <Kronic.Deth@gmail.com>
2019-03-10 10:41:56 -0500

    Fix cargo build --target wasm32-unknown-unknown

--- lumen_runtime/src/system/io.rs
+++ lumen_runtime/src/system/io.rs
@@ -1,5 +1,7 @@
+#[cfg(not(target_arch = "wasm32"))]
 use std::ffi::CStr;
 
+#[cfg(not(target_arch = "wasm32"))]
 use libc;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
@@ -8,11 +10,13 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn console_log(s: &str);
+    pub fn console_log(s: &str);
 }
 
 #[allow(dead_code)]
 #[no_mangle]
+// libc::c_char does not exist for `wasm32-unknown-unknown`
+#[cfg(not(target_arch = "wasm32"))]
 pub extern "C" fn lumen_system_io_puts(s: *const libc::c_char) {
     let sref = unsafe { CStr::from_ptr(s).to_string_lossy() };
     puts(&sref);
@@ -24,6 +28,7 @@ pub fn puts(s: &str) {
 }
 
 #[cfg(target_arch = "wasm32")]
+#[allow(dead_code)]
 pub fn puts(s: &str) {
     console_log(s);
 }

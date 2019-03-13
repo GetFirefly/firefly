945588c46365ef73ab5f8ee1e8b718f6659414c9
Luke Imhoff <Kronic.Deth@gmail.com>
2019-03-12 20:13:04 -0500

    list_to_pid
    Includes support for both local and external pids.

--- lumen_runtime/src/list.rs
+++ lumen_runtime/src/list.rs
@@ -1,9 +1,11 @@
 #![cfg_attr(not(test), allow(dead_code))]
 
 use std::cmp::Ordering;
+use std::convert::TryInto;
 
+use crate::bad_argument::BadArgument;
 use crate::process::{DebugInProcess, IntoProcess, OrderInProcess, Process};
-use crate::term::Term;
+use crate::term::{Tag::*, Term};
 
 pub type List = *const Term;
 
@@ -25,6 +27,116 @@ impl Cons {
 
     pub fn tail(&self) -> Term {
         self.tail
+    }
+
+    pub fn to_pid(&self, mut process: &mut Process) -> Result<Term, BadArgument> {
+        let prefix_tail = self.skip_pid_prefix()?;
+        let prefix_tail_cons: &Cons = prefix_tail.try_into()?;
+
+        let (node, node_tail) = prefix_tail_cons.next_decimal()?;
+        let node_tail_cons: &Cons = node_tail.try_into()?;
+
+        let first_separator_tail = node_tail_cons.skip_pid_separator()?;
+        let first_separator_tail_cons: &Cons = first_separator_tail.try_into()?;
+
+        let (number, number_tail) = first_separator_tail_cons.next_decimal()?;
+        let number_tail_cons: &Cons = number_tail.try_into()?;
+
+        let second_separator_tail = number_tail_cons.skip_pid_separator()?;
+        let second_separator_tail_cons: &Cons = second_separator_tail.try_into()?;
+
+        let (serial, serial_tail) = second_separator_tail_cons.next_decimal()?;
+        let serial_tail_cons: &Cons = serial_tail.try_into()?;
+
+        let suffix_tail = serial_tail_cons.skip_pid_suffix()?;
+
+        if suffix_tail.is_empty_list() {
+            Term::pid(node, number, serial, &mut process)
+        } else {
+            Err(bad_argument!())
+        }
+    }
+
+    const PID_PREFIX: Term = unsafe { Term::isize_to_small_integer('<' as isize) };
+    const PID_SEPARATOR: Term = unsafe { Term::isize_to_small_integer('.' as isize) };
+    const PID_SUFFIX: Term = unsafe { Term::isize_to_small_integer('>' as isize) };
+
+    fn next_decimal(&self) -> Result<(usize, Term), BadArgument> {
+        self.next_decimal_digit()
+            .and_then(&Self::rest_decimal_digits)
+    }
+
+    fn rest_decimal_digits(
+        (first_digit, first_tail): (u8, Term),
+    ) -> Result<(usize, Term), BadArgument> {
+        match first_tail.try_into() {
+            Ok(first_tail_cons) => {
+                let mut acc_decimal: usize = first_digit as usize;
+                let mut acc_tail = first_tail;
+                let mut acc_cons: &Cons = first_tail_cons;
+
+                while let Ok((digit, tail)) = acc_cons.next_decimal_digit() {
+                    acc_decimal = 10 * acc_decimal + (digit as usize);
+                    acc_tail = tail;
+
+                    match tail.try_into() {
+                        Ok(tail_cons) => acc_cons = tail_cons,
+                        Err(_) => {
+                            break;
+                        }
+                    }
+                }
+
+                Ok((acc_decimal, acc_tail))
+            }
+            Err(_) => Ok((first_digit as usize, first_tail)),
+        }
+    }
+
+    fn next_decimal_digit(&self) -> Result<(u8, Term), BadArgument> {
+        let head = self.head;
+
+        match head.tag() {
+            SmallInteger => {
+                let head_usize = unsafe { head.small_integer_to_usize() };
+
+                if head_usize <= (std::u8::MAX as usize) {
+                    let c: char = head_usize as u8 as char;
+
+                    match c.to_digit(10) {
+                        Some(digit) => Ok((digit as u8, self.tail)),
+                        None => Err(bad_argument!()),
+                    }
+                } else {
+                    Err(bad_argument!())
+                }
+            }
+            _ => Err(bad_argument!()),
+        }
+    }
+
+    fn skip_pid_prefix(&self) -> Result<Term, BadArgument> {
+        if self.head.tagged == Self::PID_PREFIX.tagged {
+            Ok(self.tail)
+        } else {
+            Err(bad_argument!())
+        }
+    }
+
+    fn skip_pid_separator(&self) -> Result<Term, BadArgument> {
+        if self.head.tagged == Self::PID_SEPARATOR.tagged {
+            Ok(self.tail)
+        } else {
+            Err(bad_argument!())
+        }
+    }
+
+    fn skip_pid_suffix(&self) -> Result<Term, BadArgument> {
+        if self.head.tagged == Self::PID_SUFFIX.tagged {
+            Ok(self.tail)
+        } else {
+            Err(bad_argument!())
+        }
     }
 }
 
--- lumen_runtime/src/otp/erlang.rs
+++ lumen_runtime/src/otp/erlang.rs
@@ -549,6 +549,12 @@ pub fn length(list: Term, mut process: &mut Process) -> Result<Term, BadArgument
     }
 }
 
+pub fn list_to_pid(string: Term, mut process: &mut Process) -> Result<Term, BadArgument> {
+    let cons: &Cons = string.try_into()?;
+
+    cons.to_pid(&mut process)
+}
+
 pub fn size(binary_or_tuple: Term, mut process: &mut Process) -> Result<Term, BadArgument> {
     match binary_or_tuple.tag() {
         Tag::Boxed => {
@@ -9223,6 +9229,219 @@ mod tests {
             assert_eq_in_process!(
                 erlang::is_tuple(subbinary_term, &mut process),
                 false_term,
+                process
+            );
+        }
+    }
+
+    mod list_to_pid {
+        use super::*;
+
+        use num_traits::Num;
+
+        #[test]
+        fn with_atom_is_bad_argument() {
+            let mut process: Process = Default::default();
+            let atom_term = Term::str_to_atom("atom", Existence::DoNotCare, &mut process).unwrap();
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(atom_term, &mut process),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_empty_list_is_bad_argument() {
+            let mut process: Process = Default::default();
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::EMPTY_LIST, &mut process),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_list_encoding_local_pid() {
+            let mut process: Process = Default::default();
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<0", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<0.", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<0.1", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<0.1.", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<0.1.2", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(
+                    Term::str_to_char_list("<0.1.2>", &mut process),
+                    &mut process
+                ),
+                Term::local_pid(1, 2),
+                process
+            );
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(
+                    Term::str_to_char_list("<0.1.2>?", &mut process),
+                    &mut process
+                ),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_list_encoding_external_pid() {
+            let mut process: Process = Default::default();
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<1", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<1.", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<1.2", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<1.2.", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+            assert_eq_in_process!(
+                erlang::list_to_pid(Term::str_to_char_list("<1.2.3", &mut process), &mut process),
+                Err(bad_argument!()),
+                process
+            );
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(
+                    Term::str_to_char_list("<1.2.3>", &mut process),
+                    &mut process
+                ),
+                Term::external_pid(1, 2, 3, &mut process),
+                process
+            );
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(
+                    Term::str_to_char_list("<1.2.3>?", &mut process),
+                    &mut process
+                ),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_small_integer_is_bad_argument() {
+            let mut process: Process = Default::default();
+            let small_integer_term = 0.into_process(&mut process);
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(small_integer_term, &mut process),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_big_integer_is_bad_argument() {
+            let mut process: Process = Default::default();
+            let big_integer_term = <BigInt as Num>::from_str_radix("576460752303423489", 10)
+                .unwrap()
+                .into_process(&mut process);
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(big_integer_term, &mut process),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_float_is_bad_argument() {
+            let mut process: Process = Default::default();
+            let float_term = 1.0.into_process(&mut process);
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(float_term, &mut process),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_tuple_is_bad_argument() {
+            let mut process: Process = Default::default();
+            let tuple_term = Term::slice_to_tuple(&[], &mut process);
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(tuple_term, &mut process),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_heap_binary_is_false() {
+            let mut process: Process = Default::default();
+            let heap_binary_term = Term::slice_to_binary(&[], &mut process);
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(heap_binary_term, &mut process),
+                Err(bad_argument!()),
+                process
+            );
+        }
+
+        #[test]
+        fn with_subbinary_is_false() {
+            let mut process: Process = Default::default();
+            let binary_term =
+                Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+            let subbinary_term = Term::subbinary(binary_term, 0, 7, 2, 1, &mut process);
+
+            assert_eq_in_process!(
+                erlang::list_to_pid(subbinary_term, &mut process),
+                Err(bad_argument!()),
                 process
             );
         }

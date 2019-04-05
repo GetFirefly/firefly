b8937c09ca87187af2d184c2b16165e890fda17f
Luke Imhoff <Kronic.Deth@gmail.com>
2019-04-05 09:03:51 -0500

    binary_part/2

---
+++ lumen_runtime/src/otp/erlang/tests/binary_part_2.rs
@@ -0,0 +1,87 @@
+use super::*;
+
+use num_traits::Num;
+
+use crate::process::IntoProcess;
+
+mod with_heap_binary;
+
+#[test]
+fn with_atom_errors_badarg() {
+    with_binary_errors_badarg(|_| Term::str_to_atom("atom", DoNotCare).unwrap());
+}
+
+#[test]
+fn with_local_reference_errors_badarg() {
+    with_binary_errors_badarg(|mut process| Term::local_reference(&mut process));
+}
+
+#[test]
+fn with_empty_list_errors_badarg() {
+    with_binary_errors_badarg(|_| Term::EMPTY_LIST);
+}
+
+#[test]
+fn with_list_errors_badarg() {
+    with_binary_errors_badarg(|mut process| list_term(&mut process));
+}
+
+#[test]
+fn with_small_integer_errors_badarg() {
+    with_binary_errors_badarg(|mut process| 0.into_process(&mut process));
+}
+
+#[test]
+fn with_big_integer_errors_badarg() {
+    with_binary_errors_badarg(|mut process| {
+        <BigInt as Num>::from_str_radix("576460752303423489", 10)
+            .unwrap()
+            .into_process(&mut process)
+    });
+}
+
+#[test]
+fn with_float_errors_badarg() {
+    with_binary_errors_badarg(|mut process| 1.0.into_process(&mut process));
+}
+
+#[test]
+fn with_local_pid_errors_badarg() {
+    with_binary_errors_badarg(|_| Term::local_pid(0, 0).unwrap());
+}
+
+#[test]
+fn with_external_pid_errors_badarg() {
+    with_binary_errors_badarg(|mut process| Term::external_pid(1, 0, 0, &mut process).unwrap());
+}
+
+#[test]
+fn with_tuple_errors_badarg() {
+    with_binary_errors_badarg(|mut process| {
+        Term::slice_to_tuple(
+            &[0.into_process(&mut process), 1.into_process(&mut process)],
+            &mut process,
+        )
+    });
+}
+
+#[test]
+fn with_map_errors_badarg() {
+    with_binary_errors_badarg(|mut process| Term::slice_to_map(&[], &mut process));
+}
+
+fn with_binary_errors_badarg<B>(binary: B)
+where
+    B: FnOnce(&mut Process) -> Term,
+{
+    errors_badarg(|mut process| {
+        let start = 0.into_process(&mut process);
+        let length = 0.into_process(&mut process);
+
+        erlang::binary_part_2(
+            binary(&mut process),
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process,
+        )
+    });
+}
---
+++ lumen_runtime/src/otp/erlang/tests/binary_part_2/with_subbinary/with_tuple_with_arity_2.rs
@@ -0,0 +1,219 @@
+use super::*;
+
+#[test]
+fn without_integer_start_without_integer_length_errors_badarg() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 2, 1, &mut process);
+        let start = Term::slice_to_tuple(
+            &[0.into_process(&mut process), 0.into_process(&mut process)],
+            &mut process,
+        );
+        let length = Term::str_to_atom("all", DoNotCare).unwrap();
+
+        assert_badarg!(erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process
+        ));
+    });
+}
+
+#[test]
+fn without_integer_start_with_integer_length_errors_badarg() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 2, 1, &mut process);
+        let start = 0.into_process(&mut process);
+        let length = Term::str_to_atom("all", DoNotCare).unwrap();
+
+        assert_badarg!(erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process
+        ));
+    });
+}
+
+#[test]
+fn with_integer_start_without_integer_length_errors_badarg() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 2, 1, &mut process);
+        let start = 0.into_process(&mut process);
+        let length = Term::str_to_atom("all", DoNotCare).unwrap();
+
+        assert_badarg!(erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process
+        ));
+    });
+}
+
+#[test]
+fn with_negative_start_with_valid_length_errors_badarg() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 2, 1, &mut process);
+        let start = (-1isize).into_process(&mut process);
+        let length = 0.into_process(&mut process);
+
+        assert_badarg!(erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process
+        ));
+    });
+}
+
+#[test]
+fn with_start_greater_than_size_with_non_negative_length_errors_badarg() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 0, 1, &mut process);
+        let start = 1.into_process(&mut process);
+        let length = 0.into_process(&mut process);
+
+        assert_badarg!(erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process
+        ));
+    });
+}
+
+#[test]
+fn with_start_less_than_size_with_negative_length_past_start_errors_badarg() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 2, 1, &mut process);
+        let start = 0.into_process(&mut process);
+        let length = (-1isize).into_process(&mut process);
+
+        assert_badarg!(erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process
+        ));
+    });
+}
+
+#[test]
+fn with_start_less_than_size_with_positive_length_past_end_errors_badarg() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 1, 1, &mut process);
+        let start = 0.into_process(&mut process);
+        let length = 2.into_process(&mut process);
+
+        assert_badarg!(erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process
+        ));
+    });
+}
+
+#[test]
+fn with_zero_start_and_byte_count_length_returns_new_subbinary_with_bytes() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 2, 1, &mut process);
+        let start = 0.into_process(&mut process);
+        let length = 2.into_process(&mut process);
+
+        assert_eq!(
+            erlang::binary_part_2(
+                subbinary,
+                Term::slice_to_tuple(&[start, length], &mut process),
+                &mut process
+            ),
+            Ok(Term::subbinary(binary, 0, 7, 2, 0, &mut process))
+        );
+    });
+}
+
+#[test]
+fn with_byte_count_start_and_negative_byte_count_length_returns_new_subbinary_with_bytes(
+) {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 2, 1, &mut process);
+        let start = 2.into_process(&mut process);
+        let length = (-2isize).into_process(&mut process);
+
+        assert_eq!(
+            erlang::binary_part_2(
+                subbinary,
+                Term::slice_to_tuple(&[start, length], &mut process),
+                &mut process
+            ),
+            Ok(Term::subbinary(binary, 0, 7, 2, 0, &mut process))
+        );
+    });
+}
+
+#[test]
+fn with_positive_start_and_negative_length_returns_subbinary() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110], &mut process);
+        let subbinary = Term::subbinary(binary, 0, 7, 1, 0, &mut process);
+        let start = 1.into_process(&mut process);
+        let length = (-1isize).into_process(&mut process);
+
+        assert_eq!(
+            erlang::binary_part_2(
+                subbinary,
+                Term::slice_to_tuple(&[start, length], &mut process),
+                &mut process
+            ),
+            Ok(Term::slice_to_binary(&[0b1111_1111], &mut process))
+        );
+
+        let returned_boxed = erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process,
+        )
+            .unwrap();
+
+        assert_eq!(returned_boxed.tag(), Boxed);
+
+        let returned_unboxed: &Term = returned_boxed.unbox_reference();
+
+        assert_eq!(returned_unboxed.tag(), Subbinary);
+    });
+}
+
+#[test]
+fn with_positive_start_and_positive_length_returns_subbinary() {
+    with_process(|mut process| {
+        let binary = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &mut process);
+        let subbinary: Term = process.subbinary(binary, 0, 7, 2, 1).into();
+        let start = 1.into_process(&mut process);
+        let length = 1.into_process(&mut process);
+
+        assert_eq!(
+            erlang::binary_part_2(
+                subbinary,
+                Term::slice_to_tuple(&[start, length], &mut process),
+                &mut process
+            ),
+            Ok(Term::slice_to_binary(&[0b0101_0101], &mut process))
+        );
+
+        let returned_boxed = erlang::binary_part_2(
+            subbinary,
+            Term::slice_to_tuple(&[start, length], &mut process),
+            &mut process,
+        )
+            .unwrap();
+
+        assert_eq!(returned_boxed.tag(), Boxed);
+
+        let returned_unboxed: &Term = returned_boxed.unbox_reference();
+
+        assert_eq!(returned_unboxed.tag(), Subbinary);
+    });
+}

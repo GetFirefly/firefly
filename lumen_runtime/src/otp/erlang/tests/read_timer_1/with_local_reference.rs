7aa721f45ca69f8fc0a20f2e38766f5202d0af46
Luke Imhoff <Kronic.Deth@gmail.com>
2019-05-06 13:37:38 -0500

    :erlang.read_timer/1

---
+++ lumen_runtime/src/otp/erlang/tests/read_timer_1/with_local_reference.rs
@@ -0,0 +1,15 @@
+use super::*;
+
+mod with_timer;
+
+#[test]
+fn without_timer_returns_false() {
+    with_process(|process| {
+        let timer_reference = Term::next_local_reference(process);
+
+        assert_eq!(
+            erlang::read_timer_1(timer_reference, process),
+            Ok(false.into())
+        );
+    });
+}

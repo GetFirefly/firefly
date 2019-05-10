4373a72b9ada56bcf21d9bd48356bdf7adeb945d
Luke Imhoff <Kronic.Deth@gmail.com>
2019-05-10 15:00:44 -0500

    Move macros into macro namespace
    Fixes annoying issues with having to explicitly order `mod` in `lib.rs`
    to have macros defined in time.

--- lumen_runtime/src/atom.rs
+++ lumen_runtime/src/atom.rs
@@ -7,16 +7,6 @@ use std::sync::{Arc, RwLock};
 
 use crate::exception::Exception;
 
-macro_rules! boolean_infix_operator {
-    ($left:ident, $right:ident, $operator:tt) => {{
-        let left_bool: bool = $left.try_into()?;
-        let right_bool: bool = $right.try_into()?;
-        let output_bool = left_bool $operator right_bool;
-
-        Ok(output_bool.into())
-    }};
-}
-
 pub enum Encoding {
     Latin1,
     Unicode,

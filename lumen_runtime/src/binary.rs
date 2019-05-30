295cdee32095332622fe92a1ce87ee3296f8f103
Luke Imhoff <Kronic.Deth@gmail.com>
2019-05-29 20:24:21 -0500

    Fix start_length_to_part_range
    Did not allow start == available_byte_count for non-negative length, but
    did for negative length.

--- lumen_runtime/src/binary.rs
+++ lumen_runtime/src/binary.rs
@@ -319,7 +319,7 @@ fn start_length_to_part_range(
     if length >= 0 {
         let non_negative_length = length as usize;
 
-        if (start < available_byte_count) & (start + non_negative_length <= available_byte_count) {
+        if (start <= available_byte_count) & (start + non_negative_length <= available_byte_count) {
             Ok(PartRange {
                 byte_offset: start,
                 byte_count: non_negative_length,

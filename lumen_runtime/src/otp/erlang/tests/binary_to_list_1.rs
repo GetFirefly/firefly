32f60ec57ba3d2215262136708e4ffece3bb656a
Luke Imhoff <Kronic.Deth@gmail.com>
2019-05-31 07:33:11 -0500

    proptest binary_to_list_1

--- lumen_runtime/src/otp/erlang/tests/binary_to_list_1.rs
+++ lumen_runtime/src/otp/erlang/tests/binary_to_list_1.rs
@@ -1,122 +1,77 @@
 use super::*;
 
-use num_traits::Num;
-
-use crate::process::IntoProcess;
+use proptest::strategy::Strategy;
 
 #[test]
-fn with_atom_errors_badarg() {
-    errors_badarg(|_| Term::str_to_atom("atom", DoNotCare).unwrap());
-}
+fn without_binary_errors_badarg() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &strategy::term::is_not_binary(arc_process.clone()),
+                |binary| {
+                    prop_assert_eq!(
+                        erlang::binary_to_list_1(binary, &arc_process),
+                        Err(badarg!())
+                    );
 
-#[test]
-fn with_local_reference_errors_badarg() {
-    errors_badarg(|process| Term::next_local_reference(process));
-}
-
-#[test]
-fn with_empty_list_errors_badarg() {
-    errors_badarg(|_| Term::EMPTY_LIST);
-}
-
-#[test]
-fn with_list_errors_badarg() {
-    errors_badarg(|process| list_term(&process));
-}
-
-#[test]
-fn with_small_integer_errors_badarg() {
-    errors_badarg(|process| 0usize.into_process(&process));
-}
-
-#[test]
-fn with_big_integer_errors_badarg() {
-    errors_badarg(|process| {
-        <BigInt as Num>::from_str_radix("576460752303423489", 10)
-            .unwrap()
-            .into_process(&process)
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
 }
 
 #[test]
-fn with_float_errors_badarg() {
-    errors_badarg(|process| 1.0.into_process(&process));
-}
+fn with_binary_returns_list_of_bytes() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &strategy::byte_vec().prop_flat_map(|byte_vec| {
+                    (
+                        Just(byte_vec.clone()),
+                        strategy::term::binary::containing_bytes(byte_vec, arc_process.clone()),
+                    )
+                }),
+                |(byte_vec, binary)| {
+                    // not using an iterator because that would too closely match the code under
+                    // test
+                    let list = match byte_vec.len() {
+                        0 => Term::EMPTY_LIST,
+                        1 => Term::cons(
+                            byte_vec[0].into_process(&arc_process),
+                            Term::EMPTY_LIST,
+                            &arc_process,
+                        ),
+                        2 => Term::cons(
+                            byte_vec[0].into_process(&arc_process),
+                            Term::cons(
+                                byte_vec[1].into_process(&arc_process),
+                                Term::EMPTY_LIST,
+                                &arc_process,
+                            ),
+                            &arc_process,
+                        ),
+                        3 => Term::cons(
+                            byte_vec[0].into_process(&arc_process),
+                            Term::cons(
+                                byte_vec[1].into_process(&arc_process),
+                                Term::cons(
+                                    byte_vec[2].into_process(&arc_process),
+                                    Term::EMPTY_LIST,
+                                    &arc_process,
+                                ),
+                                &arc_process,
+                            ),
+                            &arc_process,
+                        ),
+                        len => unimplemented!("len = {:?}", len),
+                    };
 
-#[test]
-fn with_local_pid_errors_badarg() {
-    errors_badarg(|_| Term::local_pid(0, 0).unwrap());
-}
+                    prop_assert_eq!(erlang::binary_to_list_1(binary, &arc_process), Ok(list));
 
-#[test]
-fn with_external_pid_errors_badarg() {
-    errors_badarg(|process| Term::external_pid(1, 0, 0, &process).unwrap());
-}
-
-#[test]
-fn with_tuple_errors_badarg() {
-    errors_badarg(|process| Term::slice_to_tuple(&[], &process));
-}
-
-#[test]
-fn with_map_errors_badarg() {
-    errors_badarg(|process| Term::slice_to_map(&[], &process));
-}
-
-#[test]
-fn with_heap_binary_returns_list_of_bytes() {
-    with_process(|process| {
-        let binary = Term::slice_to_binary(&[0, 1, 2], &process);
-
-        assert_eq!(
-            erlang::binary_to_list_1(binary, &process),
-            Ok(Term::cons(
-                0.into_process(&process),
-                Term::cons(
-                    1.into_process(&process),
-                    Term::cons(2.into_process(&process), Term::EMPTY_LIST, &process),
-                    &process
-                ),
-                &process
-            ))
-        );
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
-}
-
-#[test]
-fn with_subbinary_without_bit_count_returns_list_of_bytes() {
-    with_process(|process| {
-        // <<1::1, 0, 1, 2>>
-        let original = Term::slice_to_binary(&[128, 0, 129, 0b0000_0000], &process);
-        let binary = Term::subbinary(original, 0, 1, 3, 0, &process);
-
-        assert_eq!(
-            erlang::binary_to_list_1(binary, &process),
-            Ok(Term::cons(
-                0.into_process(&process),
-                Term::cons(
-                    1.into_process(&process),
-                    Term::cons(2.into_process(&process), Term::EMPTY_LIST, &process),
-                    &process
-                ),
-                &process
-            ))
-        );
-    });
-}
-
-#[test]
-fn with_subbinary_with_bit_count_errors_badarg() {
-    errors_badarg(|process| {
-        // <<1::1, 0, 1, 2>>
-        let original = Term::slice_to_binary(&[128, 0, 129, 0b0000_0000], &process);
-        Term::subbinary(original, 0, 0, 3, 1, &process)
-    });
-}
-
-fn errors_badarg<F>(binary: F)
-where
-    F: FnOnce(&Process) -> Term,
-{
-    super::errors_badarg(|process| erlang::binary_to_list_1(binary(&process), &process));
 }

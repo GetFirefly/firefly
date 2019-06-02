bad0380cb18a0d15c740b6f73edda1273e25960d
Luke Imhoff <Kronic.Deth@gmail.com>
2019-06-01 21:16:54 -0500

    proptest bxor_2

--- lumen_runtime/src/otp/erlang/tests/bor_2.rs
+++ lumen_runtime/src/otp/erlang/tests/bor_2.rs
@@ -4,7 +4,26 @@ mod with_big_integer_left;
 mod with_small_integer_left;
 
 #[test]
-fn without_integer_right_errors_badarith() {
+fn without_integer_left_errors_badarith() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    strategy::term::is_not_integer(arc_process.clone()),
+                    strategy::term::is_integer(arc_process.clone()),
+                ),
+                |(left, right)| {
+                    prop_assert_eq!(erlang::bor_2(left, right, &arc_process), Err(badarith!()));
+
+                    Ok(())
+                },
+            )
+            .unwrap();
+    });
+}
+
+#[test]
+fn with_integer_left_without_integer_right_errors_badarith() {
     with_process_arc(|arc_process| {
         TestRunner::new(Config::with_source_file(file!()))
             .run(

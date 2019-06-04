0ebf31aed8e7cb3f310ee668b5b0ba429910b7e1
Luke Imhoff <Kronic.Deth@gmail.com>
2019-06-03 22:06:22 -0500

    proptest div_2

--- lumen_runtime/src/otp/erlang/tests/div_2.rs
+++ lumen_runtime/src/otp/erlang/tests/div_2.rs
@@ -1,72 +1,70 @@
 use super::*;
 
 mod with_big_integer_dividend;
-mod with_float_dividend;
 mod with_small_integer_dividend;
 
 #[test]
-fn with_atom_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|_| Term::str_to_atom("dividend", DoNotCare).unwrap());
-}
+fn without_integer_dividend_errors_badarith() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    strategy::term::is_not_integer(arc_process.clone()),
+                    strategy::term::is_integer(arc_process.clone()),
+                ),
+                |(dividend, divisor)| {
+                    prop_assert_eq!(
+                        erlang::div_2(dividend, divisor, &arc_process),
+                        Err(badarith!())
+                    );
 
-#[test]
-fn with_local_reference_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|process| Term::next_local_reference(process));
-}
-
-#[test]
-fn with_empty_list_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|_| Term::EMPTY_LIST);
-}
-
-#[test]
-fn with_list_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|process| {
-        Term::cons(0.into_process(&process), 1.into_process(&process), &process)
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
 }
 
 #[test]
-fn with_local_pid_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|_| Term::local_pid(0, 1).unwrap());
-}
+fn with_integer_dividend_without_integer_divisor_errors_badarith() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    strategy::term::is_integer(arc_process.clone()),
+                    strategy::term::is_not_integer(arc_process.clone()),
+                ),
+                |(dividend, divisor)| {
+                    prop_assert_eq!(
+                        erlang::div_2(dividend, divisor, &arc_process),
+                        Err(badarith!())
+                    );
 
-#[test]
-fn with_external_pid_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|process| Term::external_pid(1, 2, 3, &process).unwrap());
-}
-
-#[test]
-fn with_tuple_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|process| Term::slice_to_tuple(&[], &process));
-}
-
-#[test]
-fn with_map_is_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|process| Term::slice_to_map(&[], &process));
-}
-
-#[test]
-fn with_heap_binary_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|process| Term::slice_to_binary(&[], &process));
-}
-
-#[test]
-fn with_subbinary_dividend_errors_badarith() {
-    with_dividend_errors_badarith(|process| {
-        let original = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &process);
-        Term::subbinary(original, 0, 7, 2, 1, &process)
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
 }
 
-fn with_dividend_errors_badarith<M>(dividend: M)
-where
-    M: FnOnce(&Process) -> Term,
-{
-    super::errors_badarith(|process| {
-        let dividend = dividend(&process);
-        let divisor = 0.into_process(&process);
+#[test]
+fn with_integer_dividend_with_zero_divisor_errors_badarith() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    strategy::term::is_integer(arc_process.clone()),
+                    Just(0.into_process(&arc_process)),
+                ),
+                |(dividend, divisor)| {
+                    prop_assert_eq!(
+                        erlang::div_2(dividend, divisor, &arc_process),
+                        Err(badarith!())
+                    );
 
-        erlang::div_2(dividend, divisor, &process)
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
 }

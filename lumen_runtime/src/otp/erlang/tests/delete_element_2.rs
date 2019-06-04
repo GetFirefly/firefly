280d8b2f53e610046c71c7994342d483b77cf4eb
Luke Imhoff <Kronic.Deth@gmail.com>
2019-06-04 09:06:41 -0500

    proptest element_2

--- lumen_runtime/src/otp/erlang/tests/delete_element_2.rs
+++ lumen_runtime/src/otp/erlang/tests/delete_element_2.rs
@@ -1,7 +1,5 @@
 use super::*;
 
-use proptest::strategy::Strategy;
-
 #[test]
 fn without_tuple_errors_badarg() {
     with_process_arc(|arc_process| {
@@ -29,24 +27,7 @@ fn with_tuple_without_integer_between_1_and_the_length_inclusive_errors_badarg()
     with_process_arc(|arc_process| {
         TestRunner::new(Config::with_source_file(file!()))
             .run(
-                &((
-                    strategy::term::tuple(arc_process.clone()),
-                    strategy::term(arc_process.clone()),
-                )
-                    .prop_filter("Index either needs to not be an integer or not be an integer in the index range 1..=len", |(tuple, index)| {
-                        let index_big_int_result: std::result::Result<BigInt, _> = index.try_into();
-
-                        match index_big_int_result {
-                            Ok(index_big_int) => {
-                                let tuple_tuple: &Tuple = tuple.unbox_reference();
-                                let min_index: BigInt = 1.into();
-                                let max_index: BigInt = tuple_tuple.len().into();
-
-                                !((min_index <= index_big_int) && (index_big_int <= max_index))
-                            }
-                            _ => true,
-                        }
-                    })),
+                &strategy::term::tuple::without_index(arc_process.clone()),
                 |(tuple, index)| {
                     prop_assert_eq!(
                         erlang::delete_element_2(tuple, index, &arc_process),
@@ -65,24 +46,7 @@ fn with_tuple_with_integer_between_1_and_the_length_inclusive_returns_tuple_with
     with_process_arc(|arc_process| {
         TestRunner::new(Config::with_source_file(file!()))
             .run(
-                &(1_usize..=4_usize)
-                    .prop_flat_map(|len| {
-                        (
-                            proptest::collection::vec(
-                                strategy::term(arc_process.clone()),
-                                len..=len,
-                            ),
-                            0..len,
-                        )
-                    })
-                    .prop_map(|(element_vec, zero_based_index)| {
-                        (
-                            element_vec.clone(),
-                            zero_based_index,
-                            Term::slice_to_tuple(&element_vec, &arc_process),
-                            (zero_based_index + 1).into_process(&arc_process),
-                        )
-                    }),
+                &strategy::term::tuple::with_index(arc_process.clone()),
                 |(mut element_vec, element_vec_index, tuple, index)| {
                     element_vec.remove(element_vec_index);
 
--- lumen_runtime/src/otp/erlang/tests/strategy/term/tuple.rs
+++ lumen_runtime/src/otp/erlang/tests/strategy/term/tuple.rs
@@ -1,10 +1,14 @@
+use std::convert::TryInto;
 use std::sync::Arc;
 
-use proptest::collection::SizeRange;
-use proptest::strategy::Strategy;
+use num_bigint::BigInt;
 
-use crate::process::Process;
+use proptest::collection::SizeRange;
+use proptest::strategy::{Just, Strategy};
+
+use crate::process::{IntoProcess, Process};
 use crate::term::Term;
+use crate::tuple::Tuple;
 
 pub fn intermediate(
     element: impl Strategy<Value = Term>,
@@ -13,4 +17,43 @@ pub fn intermediate(
 ) -> impl Strategy<Value = Term> {
     proptest::collection::vec(element, size_range)
         .prop_map(move |vec| Term::slice_to_tuple(&vec, &arc_process))
+}
+
+pub fn with_index(
+    arc_process: Arc<Process>,
+) -> impl Strategy<Value = (Vec<Term>, usize, Term, Term)> {
+    (Just(arc_process), 1_usize..=4_usize)
+        .prop_flat_map(|(arc_process, len)| {
+            (
+                Just(arc_process.clone()),
+                proptest::collection::vec(super::super::term(arc_process), len..=len),
+                0..len,
+            )
+        })
+        .prop_map(|(arc_process, element_vec, zero_based_index)| {
+            (
+                element_vec.clone(),
+                zero_based_index,
+                Term::slice_to_tuple(&element_vec, &arc_process),
+                (zero_based_index + 1).into_process(&arc_process),
+            )
+        })
+}
+
+pub fn without_index(arc_process: Arc<Process>) -> impl Strategy<Value = (Term, Term)> {
+    (super::tuple(arc_process.clone()), super::super::term(arc_process.clone()))
+        .prop_filter("Index either needs to not be an integer or not be an integer in the index range 1..=len", |(tuple, index)| {
+            let index_big_int_result: std::result::Result<BigInt, _> = index.try_into();
+
+            match index_big_int_result {
+                Ok(index_big_int) => {
+                    let tuple_tuple: &Tuple = tuple.unbox_reference();
+                    let min_index: BigInt = 1.into();
+                    let max_index: BigInt = tuple_tuple.len().into();
+
+                    !((min_index <= index_big_int) && (index_big_int <= max_index))
+                }
+                _ => true,
+            }
+        })
 }

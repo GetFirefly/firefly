2085def6397b2d35548af1174d092fbde0fc8bc1
Luke Imhoff <Kronic.Deth@gmail.com>
2019-06-10 08:43:40 +0200

    proptest negate_1

--- lumen_runtime/src/otp/erlang/tests/negate_1.rs
+++ lumen_runtime/src/otp/erlang/tests/negate_1.rs
@@ -1,96 +1,58 @@
 use super::*;
 
-use crate::process::IntoProcess;
+use proptest::prop_oneof;
+use proptest::strategy::Strategy;
 
 #[test]
-fn with_atom_errors_badarith() {
-    errors_badarith(|_| Term::str_to_atom("number", DoNotCare).unwrap());
-}
+fn without_number_errors_badarith() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &strategy::term::is_not_number(arc_process.clone()),
+                |number| {
+                    prop_assert_eq!(erlang::negate_1(number, &arc_process), Err(badarith!()));
 
-#[test]
-fn with_local_reference_errors_badarith() {
-    errors_badarith(|process| Term::next_local_reference(process));
-}
-
-#[test]
-fn with_empty_list_errors_badarith() {
-    errors_badarith(|_| Term::EMPTY_LIST);
-}
-
-#[test]
-fn with_list_errors_badarith() {
-    errors_badarith(|process| list_term(&process));
-}
-
-#[test]
-fn with_small_integer_returns_small_integer() {
-    with_process(|process| {
-        assert_eq!(
-            erlang::negate_1(1.into_process(&process), &process),
-            Ok((-1_isize).into_process(&process))
-        );
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
 }
 
 #[test]
-fn with_big_integer_returns_big_integer() {
-    with_process(|process| {
-        assert_eq!(
-            erlang::negate_1(
-                (crate::integer::small::MIN - 1).into_process(&process),
-                &process
-            ),
-            Ok((crate::integer::small::MAX + 2).into_process(&process))
-        );
+fn with_integer_returns_integer_of_opposite_sign() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &prop_oneof![std::isize::MIN..=-1, 1..=std::isize::MAX]
+                    .prop_map(|i| (i.into_process(&arc_process), i)),
+                |(number, i)| {
+                    let negated = (-i).into_process(&arc_process);
+
+                    prop_assert_eq!(erlang::negate_1(number, &arc_process), Ok(negated));
+
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
 }
 
 #[test]
-fn with_float_returns_argument() {
-    with_process(|process| {
-        assert_eq!(
-            erlang::negate_1((-1.0).into_process(&process), &process),
-            Ok(1.0.into_process(&process))
-        );
+fn with_float_returns_float_of_opposite_sign() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &prop_oneof![std::f64::MIN..=-1.0, 1.0..=std::f64::MAX]
+                    .prop_map(|f| (f.into_process(&arc_process), f)),
+                |(number, f)| {
+                    let negated = (-f).into_process(&arc_process);
+
+                    prop_assert_eq!(erlang::negate_1(number, &arc_process), Ok(negated));
+
+                    Ok(())
+                },
+            )
+            .unwrap();
     });
-}
-
-#[test]
-fn with_local_pid_errors_badarith() {
-    errors_badarith(|_| Term::local_pid(0, 0).unwrap());
-}
-
-#[test]
-fn with_external_pid_errors_badarith() {
-    errors_badarith(|process| Term::external_pid(1, 0, 0, &process).unwrap());
-}
-
-#[test]
-fn with_tuple_errors_badarith() {
-    errors_badarith(|process| Term::slice_to_tuple(&[], &process));
-}
-
-#[test]
-fn with_map_errors_badarith() {
-    errors_badarith(|process| Term::slice_to_map(&[], &process));
-}
-
-#[test]
-fn with_heap_binary_errors_badarith() {
-    errors_badarith(|process| Term::slice_to_binary(&[], &process));
-}
-
-#[test]
-fn with_subbinary_errors_badarith() {
-    errors_badarith(|process| {
-        let original = Term::slice_to_binary(&[0b0000_00001, 0b1111_1110, 0b1010_1011], &process);
-        Term::subbinary(original, 0, 7, 2, 1, &process)
-    });
-}
-
-fn errors_badarith<N>(number: N)
-where
-    N: FnOnce(&Process) -> Term,
-{
-    super::errors_badarith(|process| erlang::negate_1(number(&process), &process));
 }

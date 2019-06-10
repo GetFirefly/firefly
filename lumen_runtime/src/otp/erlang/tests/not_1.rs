011684f2c9825d3baa6d494bf593429c5e495f8f
Luke Imhoff <Kronic.Deth@gmail.com>
2019-06-10 08:49:49 +0200

    proptest not_1

--- lumen_runtime/src/otp/erlang/tests/not_1.rs
+++ lumen_runtime/src/otp/erlang/tests/not_1.rs
@@ -1,8 +1,19 @@
 use super::*;
 
 #[test]
-fn with_atom_errors_badarg() {
-    errors_badarg(|_| Term::str_to_atom("atom", DoNotCare).unwrap());
+fn without_boolean_errors_badarg() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &strategy::term::is_not_boolean(arc_process.clone()),
+                |boolean| {
+                    prop_assert_eq!(erlang::not_1(boolean), Err(badarg!()));
+
+                    Ok(())
+                },
+            )
+            .unwrap();
+    });
 }
 
 #[test]
@@ -13,71 +24,4 @@ fn with_false_returns_true() {
 #[test]
 fn with_true_returns_false() {
     assert_eq!(erlang::not_1(true.into()), Ok(false.into()));
-}
-
-#[test]
-fn with_local_reference_errors_badarg() {
-    errors_badarg(|process| Term::next_local_reference(process));
-}
-
-#[test]
-fn with_heap_binary_errors_badarg() {
-    errors_badarg(|process| Term::slice_to_binary(&[0], &process));
-}
-
-#[test]
-fn with_subbinary_errors_badarg() {
-    errors_badarg(|process| bitstring!(1 :: 1, &process));
-}
-
-#[test]
-fn with_empty_list_errors_badarg() {
-    errors_badarg(|_| Term::EMPTY_LIST);
-}
-
-#[test]
-fn with_list_errors_badarg() {
-    errors_badarg(|process| list_term(&process));
-}
-
-#[test]
-fn with_small_integer_errors_badarg() {
-    errors_badarg(|process| 1.into_process(&process));
-}
-
-#[test]
-fn with_big_integer_errors_badarg() {
-    errors_badarg(|process| (crate::integer::small::MAX + 1).into_process(&process));
-}
-
-#[test]
-fn with_float_errors_badarg() {
-    errors_badarg(|process| 1.0.into_process(&process));
-}
-
-#[test]
-fn with_local_pid_errors_badarg() {
-    errors_badarg(|_| Term::local_pid(0, 0).unwrap());
-}
-
-#[test]
-fn with_external_pid_errors_badarg() {
-    errors_badarg(|process| Term::external_pid(1, 0, 0, &process).unwrap());
-}
-
-#[test]
-fn with_tuple_errors_badarg() {
-    errors_badarg(|process| Term::slice_to_tuple(&[], &process));
-}
-
-#[test]
-fn with_map_errors_badarg() {
-    errors_badarg(|process| Term::slice_to_map(&[], &process));
-}
-
-fn errors_badarg<I>(integer: I)
-where
-    I: FnOnce(&Process) -> Term,
-{
-    super::errors_badarg(|process| erlang::not_1(integer(&process)));
 }

67e53c05b2c51aef3cd2db8315ce91cb67df2988
Luke Imhoff <Kronic.Deth@gmail.com>
2019-07-23 23:00:11 -0500

    Fix AllocErr in tuple_size_1 tests

--- lumen_runtime/src/otp/erlang/tests/tuple_size_1.rs
+++ lumen_runtime/src/otp/erlang/tests/tuple_size_1.rs
@@ -4,21 +4,24 @@ use proptest::strategy::Strategy;
 
 #[test]
 fn without_tuple_errors_badarg() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &strategy::term::is_not_tuple(arc_process.clone()),
-                |tuple| {
-                    prop_assert_eq!(
-                        erlang::tuple_size_1(tuple, &arc_process),
-                        Err(badarg!().into())
-                    );
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                (
+                    Just(arc_process.clone()),
+                    strategy::term::is_not_tuple(arc_process),
+                )
+            }),
+            |(arc_process, tuple)| {
+                prop_assert_eq!(
+                    erlang::tuple_size_1(tuple, &arc_process),
+                    Err(badarg!().into())
+                );
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }
 
 #[test]

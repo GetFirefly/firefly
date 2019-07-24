6540e0e1a941b0d410e48e2d4f89791b2280a60e
Luke Imhoff <Kronic.Deth@gmail.com>
2019-07-23 22:52:01 -0500

    Fix AllocErr for tuple_to_size_1 tests

--- lumen_runtime/src/otp/erlang/tests/tuple_to_list_1.rs
+++ lumen_runtime/src/otp/erlang/tests/tuple_to_list_1.rs
@@ -2,21 +2,24 @@ use super::*;
 
 #[test]
 fn without_tuple_errors_badarg() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &strategy::term::is_not_tuple(arc_process.clone()),
-                |tuple| {
-                    prop_assert_eq!(
-                        erlang::tuple_to_list_1(tuple, &arc_process),
-                        Err(badarg!().into())
-                    );
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                (
+                    Just(arc_process.clone()),
+                    strategy::term::is_not_tuple(arc_process),
+                )
+            }),
+            |(arc_process, tuple)| {
+                prop_assert_eq!(
+                    erlang::tuple_to_list_1(tuple, &arc_process),
+                    Err(badarg!().into())
+                );
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }
 
 #[test]

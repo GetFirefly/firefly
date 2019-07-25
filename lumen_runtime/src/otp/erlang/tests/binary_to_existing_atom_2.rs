9602ac03f90b8f1d4a0491bc2b70d237a84f28b0
Luke Imhoff <Kronic.Deth@gmail.com>
2019-07-24 19:00:40 -0500

    Add ProcBin support to binary_to_existing_atom_2 test

--- lumen_runtime/src/otp/erlang/tests/binary_to_existing_atom_2.rs
+++ lumen_runtime/src/otp/erlang/tests/binary_to_existing_atom_2.rs
@@ -84,6 +84,9 @@ fn with_utf8_binary_with_valid_encoding_with_existing_atom_returns_atom() {
                     let byte_vec: Vec<u8> = match binary.to_typed_term().unwrap() {
                         TypedTerm::Boxed(boxed) => match boxed.to_typed_term().unwrap() {
                             TypedTerm::HeapBinary(heap_binary) => heap_binary.as_bytes().to_vec(),
+                            TypedTerm::ProcBin(process_binary) => {
+                                process_binary.as_bytes().to_vec()
+                            }
                             TypedTerm::SubBinary(subbinary) => subbinary.full_byte_iter().collect(),
                             unboxed_typed_term => {
                                 panic!("unboxed_typed_term = {:?}", unboxed_typed_term)

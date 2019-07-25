08c4e3669f019b28f13342ba606523b988dcd9d6
Luke Imhoff <Kronic.Deth@gmail.com>
2019-07-24 20:47:55 -0500

    Fix AllocErr in binary_part_2 test

--- lumen_runtime/src/otp/erlang/tests/binary_part_2/with_bitstring.rs
+++ lumen_runtime/src/otp/erlang/tests/binary_part_2/with_bitstring.rs
@@ -6,24 +6,25 @@ mod with_tuple_with_arity_2;
 
 #[test]
 fn without_tuple_start_length_errors_badarg() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &(
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                (
+                    Just(arc_process.clone()),
                     strategy::term::is_bitstring(arc_process.clone()),
-                    strategy::term::is_not_tuple(arc_process.clone()),
-                ),
-                |(binary, start_length)| {
-                    prop_assert_eq!(
-                        erlang::binary_part_2(binary, start_length, &arc_process),
-                        Err(badarg!().into())
-                    );
+                    strategy::term::is_not_tuple(arc_process),
+                )
+            }),
+            |(arc_process, binary, start_length)| {
+                prop_assert_eq!(
+                    erlang::binary_part_2(binary, start_length, &arc_process),
+                    Err(badarg!().into())
+                );
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }
 
 #[test]

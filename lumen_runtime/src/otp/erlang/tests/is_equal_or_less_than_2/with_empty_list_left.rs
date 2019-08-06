1352febcabeda3e8077127551d7857d12105a13f
Luke Imhoff <Kronic.Deth@gmail.com>
2019-08-06 14:52:14 -0500

    Fix Alloc error in is_equal_or_less_than_2 test

--- lumen_runtime/src/otp/erlang/tests/is_equal_or_less_than_2/with_empty_list_left.rs
+++ lumen_runtime/src/otp/erlang/tests/is_equal_or_less_than_2/with_empty_list_left.rs
@@ -25,21 +25,21 @@ fn without_list_or_bitstring_returns_false() {
 
 #[test]
 fn with_list_or_bitstring_right_returns_true() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &strategy::term(arc_process.clone())
-                    .prop_filter("Right cannot be a list or bitstring", |right| {
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                strategy::term(arc_process.clone())
+                    .prop_filter("Right must be a list or bitstring", |right| {
                         right.is_list() || right.is_bitstring()
-                    }),
-                |right| {
-                    let left = Term::NIL;
+                    })
+            }),
+            |right| {
+                let left = Term::NIL;
 
-                    prop_assert_eq!(erlang::is_equal_or_less_than_2(left, right), true.into());
+                prop_assert_eq!(erlang::is_equal_or_less_than_2(left, right), true.into());
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }

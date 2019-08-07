a8aeb84e6acf04320c1db2a4cedccf4153ae5545
Luke Imhoff <Kronic.Deth@gmail.com>
2019-08-06 21:12:32 -0500

    Fix Alloc errors in are_not_equal_after_conversion_2 tests

--- lumen_runtime/src/otp/erlang/tests/are_not_equal_after_conversion_2/with_map_left.rs
+++ lumen_runtime/src/otp/erlang/tests/are_not_equal_after_conversion_2/with_map_left.rs
@@ -4,51 +4,52 @@ use proptest::strategy::Strategy;
 
 #[test]
 fn without_map_right_returns_true() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &(
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                (
                     strategy::term::map(arc_process.clone()),
                     strategy::term(arc_process.clone())
                         .prop_filter("Right cannot be a map", |right| !right.is_map()),
-                ),
-                |(left, right)| {
-                    prop_assert_eq!(
-                        erlang::are_not_equal_after_conversion_2(left, right),
-                        true.into()
-                    );
+                )
+            }),
+            |(left, right)| {
+                prop_assert_eq!(
+                    erlang::are_not_equal_after_conversion_2(left, right),
+                    true.into()
+                );
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }
 
 #[test]
 fn with_same_map_right_returns_false() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(&strategy::term::map(arc_process.clone()), |operand| {
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process()
+                .prop_flat_map(|arc_process| strategy::term::map(arc_process.clone())),
+            |operand| {
                 prop_assert_eq!(
                     erlang::are_not_equal_after_conversion_2(operand, operand),
                     false.into()
                 );
 
                 Ok(())
-            })
-            .unwrap();
-    });
+            },
+        )
+        .unwrap();
 }
 
 #[test]
 fn with_same_value_map_right_returns_false() {
-    with_process_arc(|arc_process| {
-        let key_or_value = strategy::term(arc_process.clone());
-
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &proptest::collection::hash_map(
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                let key_or_value = strategy::term(arc_process.clone());
+                proptest::collection::hash_map(
                     key_or_value.clone(),
                     key_or_value,
                     strategy::size_range(),
@@ -61,39 +62,39 @@ fn with_same_value_map_right_returns_false() {
                         heap.map_from_slice(&entry_vec).unwrap(),
                         heap.map_from_slice(&entry_vec).unwrap(),
                     )
-                }),
-                |(left, right)| {
-                    prop_assert_eq!(
-                        erlang::are_not_equal_after_conversion_2(left, right),
-                        false.into()
-                    );
+                })
+            }),
+            |(left, right)| {
+                prop_assert_eq!(
+                    erlang::are_not_equal_after_conversion_2(left, right),
+                    false.into()
+                );
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }
 
 #[test]
 fn with_different_map_right_returns_true() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &(
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                (
                     strategy::term::map(arc_process.clone()),
                     strategy::term::map(arc_process.clone()),
                 )
-                    .prop_filter("Maps must be different", |(left, right)| left != right),
-                |(left, right)| {
-                    prop_assert_eq!(
-                        erlang::are_not_equal_after_conversion_2(left, right),
-                        true.into()
-                    );
+                    .prop_filter("Maps must be different", |(left, right)| left != right)
+            }),
+            |(left, right)| {
+                prop_assert_eq!(
+                    erlang::are_not_equal_after_conversion_2(left, right),
+                    true.into()
+                );
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }
--- lumen_runtime/src/otp/erlang/tests/are_not_equal_after_conversion_2/with_tuple_left.rs
+++ lumen_runtime/src/otp/erlang/tests/are_not_equal_after_conversion_2/with_tuple_left.rs
@@ -4,25 +4,25 @@ use proptest::strategy::Strategy;
 
 #[test]
 fn without_list_right_returns_true() {
-    with_process_arc(|arc_process| {
-        TestRunner::new(Config::with_source_file(file!()))
-            .run(
-                &(
+    TestRunner::new(Config::with_source_file(file!()))
+        .run(
+            &strategy::process().prop_flat_map(|arc_process| {
+                (
                     strategy::term::tuple(arc_process.clone()),
                     strategy::term(arc_process.clone())
                         .prop_filter("Right must not be tuple", |v| !v.is_tuple()),
-                ),
-                |(left, right)| {
-                    prop_assert_eq!(
-                        erlang::are_not_equal_after_conversion_2(left, right),
-                        true.into()
-                    );
+                )
+            }),
+            |(left, right)| {
+                prop_assert_eq!(
+                    erlang::are_not_equal_after_conversion_2(left, right),
+                    true.into()
+                );
 
-                    Ok(())
-                },
-            )
-            .unwrap();
-    });
+                Ok(())
+            },
+        )
+        .unwrap();
 }
 
 #[test]

718316ea63f9ca2fa1926bd97324882c04a948da
Luke Imhoff <Kronic.Deth@gmail.com>
2019-08-13 14:17:17 -0500

    cfg out tests that need proptest on wasm32

--- lumen_runtime/src/test.rs
+++ lumen_runtime/src/test.rs
@@ -1,1 +1,6 @@
+// wasm32 proptest cannot be compiled at the same time as non-wasm32 proptest, so disable tests that
+// use proptest completely for wasm32
+//
+// See https://github.com/rust-lang/cargo/issues/4866
+#[cfg(all(not(target_arch = "wasm32"), test))]
 pub mod strategy;

53df003fd7f97066dc0b0cfb3889d6836c7090bf
Luke Imhoff <Kronic.Deth@gmail.com>
2019-08-16 17:45:53 -0500

    Ignore flaky timer test

--- lumen_runtime/src/otp/erlang/tests/read_timer_2/with_reference/with_list_options/without_async/with_timer/in_same_thread.rs
+++ lumen_runtime/src/otp/erlang/tests/read_timer_2/with_reference/with_list_options/without_async/with_timer/in_same_thread.rs
@@ -22,6 +22,7 @@ fn without_timeout_returns_milliseconds_remaining_and_does_not_send_timeout_mess
                 .expect("Timer could not be read");
 
         assert!(first_milliseconds_remaining.is_integer());
+        // flaky
         assert!(process.integer(0).unwrap() < first_milliseconds_remaining);
         assert!(first_milliseconds_remaining <= process.integer(milliseconds / 2).unwrap());
 

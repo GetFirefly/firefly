476fd0c0ae201ff29ce887d9e88e645a132694f4
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2019-08-26 04:05:43 -0400

    Add offset_of helper

--- liblumen_core/src/util.rs
+++ liblumen_core/src/util.rs
@@ -3,3 +3,22 @@
 pub mod cache_padded;
 pub mod pointer;
 pub mod reference;
+
+#[macro_export]
+macro_rules! offset_of {
+    ($strukt:path, $field:ident) => ({
+        // Using a separate function to minimize unhygienic hazards
+        // (e.g. unsafety of #[repr(packed)] field borrows).
+        // Uncomment `const` when `const fn`s can juggle pointers.
+        fn offset() -> usize {
+            let u = core::mem::MaybeUninit::<$strukt>::uninit();
+            // Use pattern-matching to avoid accidentally going through Deref.
+            let &$strukt { $field: ref f, .. } = unsafe { &*u.as_ptr() };
+            let o = (f as *const _ as usize).wrapping_sub(&u as *const _ as usize);
+            // Triple check that we are within `u` still.
+            assert!((0..=core::mem::size_of_val(&u)).contains(&o));
+            o
+        }
+        offset()
+    })
+}

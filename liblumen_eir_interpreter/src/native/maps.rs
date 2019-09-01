3673787ea97ed0125e5ec3a3f8c2f85e338c24d0
Nate Heydt <nate@tinfoilsecurity.com>
2019-09-01 06:10:11 -0700

    Implement :maps.find/2

--- liblumen_eir_interpreter/src/native/maps.rs
+++ liblumen_eir_interpreter/src/native/maps.rs
@@ -8,6 +8,11 @@ use crate::module::NativeModule;
 pub fn make_maps() -> NativeModule {
     let mut native = NativeModule::new(Atom::try_from_str("maps").unwrap());
 
+    native.add_simple(Atom::try_from_str("find").unwrap(), 2, |proc, args| {
+        assert!(args.len() == 2);
+        Ok(maps::find_2::native(proc, args[0], args[1]).unwrap())
+    });
+
     native.add_simple(Atom::try_from_str("get").unwrap(), 3, |proc, args| {
         assert!(args.len() == 3);
         Ok(maps::get_3::native(proc, args[0], args[1], args[2]).unwrap())

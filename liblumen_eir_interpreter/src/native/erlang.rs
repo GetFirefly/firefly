da47ffd9461fd4805210bde86e892b5f52127db6
Zach Daniel <zachary.s.daniel@gmail.com>
2019-09-11 14:05:10 -0400

    register `/` and `*` BIFS to the interpreter

--- liblumen_eir_interpreter/src/native/erlang.rs
+++ liblumen_eir_interpreter/src/native/erlang.rs
@@ -7,6 +7,14 @@ use crate::module::NativeModule;
 pub fn make_erlang() -> NativeModule {
     let mut native = NativeModule::new(Atom::try_from_str("erlang").unwrap());
 
+    native.add_simple(Atom::try_from_str("*").unwrap(), 2, |proc, args| {
+        erlang::multiply_2(args[0], args[1], proc)
+    });
+
+    native.add_simple(Atom::try_from_str("/").unwrap(), 2, |proc, args| {
+        erlang::div_2(args[0], args[1], proc)
+    });
+
     native.add_simple(Atom::try_from_str("<").unwrap(), 2, |_proc, args| {
         Ok(erlang::is_less_than_2(args[0], args[1]))
     });

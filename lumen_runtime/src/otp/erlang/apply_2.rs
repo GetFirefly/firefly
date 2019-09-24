a37c7fa3a41555307843d63e45e29e83dee036a5
Luke Imhoff <Kronic.Deth@gmail.com>
2019-09-24 13:19:42 -0500

    apply/2
    Resolves #136

---
+++ lumen_runtime/src/otp/erlang/apply_2.rs
@@ -0,0 +1,110 @@
+// wasm32 proptest cannot be compiled at the same time as non-wasm32 proptest, so disable tests that
+// use proptest completely for wasm32
+//
+// See https://github.com/rust-lang/cargo/issues/4866
+#[cfg(all(not(target_arch = "wasm32"), test))]
+mod test;
+
+use std::convert::TryInto;
+use std::sync::Arc;
+
+use liblumen_alloc::erts::exception;
+use liblumen_alloc::erts::exception::system::Alloc;
+use liblumen_alloc::erts::process::code;
+use liblumen_alloc::erts::process::code::stack::frame::{Frame, Placement};
+use liblumen_alloc::erts::process::Process;
+use liblumen_alloc::erts::term::{Atom, Boxed, Closure, Term, TypedTerm};
+use liblumen_alloc::{badarg, badarity};
+
+use liblumen_alloc::ModuleFunctionArity;
+
+pub fn place_frame_with_arguments(
+    process: &Process,
+    placement: Placement,
+    function: Term,
+    arguments: Term,
+) -> Result<(), Alloc> {
+    process.stack_push(arguments)?;
+    process.stack_push(function)?;
+    process.place_frame(frame(), placement);
+
+    Ok(())
+}
+
+// Private
+
+fn code(arc_process: &Arc<Process>) -> code::Result {
+    arc_process.reduce();
+
+    let function = arc_process.stack_pop().unwrap();
+    let arguments = arc_process.stack_pop().unwrap();
+
+    let function_result_boxed_closure: Result<Boxed<Closure>, _> = function.try_into();
+
+    match function_result_boxed_closure {
+        Ok(function_boxed_closure) => {
+            let mut argument_vec = Vec::new();
+
+            match arguments.to_typed_term().unwrap() {
+                TypedTerm::Nil => (),
+                TypedTerm::List(argument_boxed_cons) => {
+                    for result in argument_boxed_cons.into_iter() {
+                        match result {
+                            Ok(element) => argument_vec.push(element),
+                            Err(_) => {
+                                arc_process.exception(badarg!());
+
+                                return Ok(());
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    arc_process.exception(badarg!());
+
+                    return Ok(());
+                }
+            }
+
+            if argument_vec.len() == (function_boxed_closure.arity() as usize) {
+                function_boxed_closure.place_frame_with_arguments(
+                    arc_process,
+                    Placement::Replace,
+                    argument_vec,
+                )?;
+
+                Process::call_code(arc_process)
+            } else {
+                match badarity!(arc_process, function, arguments) {
+                    exception::Exception::Runtime(runtime_exception) => {
+                        arc_process.exception(runtime_exception);
+
+                        Ok(())
+                    }
+                    exception::Exception::System(system_exception) => Err(system_exception),
+                }
+            }
+        }
+        Err(_) => {
+            arc_process.exception(badarg!());
+
+            Ok(())
+        }
+    }
+}
+
+fn frame() -> Frame {
+    Frame::new(module_function_arity(), code)
+}
+
+fn function() -> Atom {
+    Atom::try_from_str("apply").unwrap()
+}
+
+fn module_function_arity() -> Arc<ModuleFunctionArity> {
+    Arc::new(ModuleFunctionArity {
+        module: super::module(),
+        function: function(),
+        arity: 2,
+    })
+}
---
+++ lumen_runtime/src/otp/erlang/apply_2/test/with_function/with_empty_list_arguments.rs
@@ -0,0 +1,116 @@
+use super::*;
+
+use std::sync::Arc;
+
+use proptest::strategy::Strategy;
+
+use liblumen_alloc::badarity;
+use liblumen_alloc::erts::process::code::Code;
+use liblumen_alloc::erts::process::Process;
+use liblumen_alloc::erts::term::atom_unchecked;
+use liblumen_alloc::erts::ModuleFunctionArity;
+
+use crate::test::strategy::term::closure;
+
+#[test]
+fn without_arity_errors_badarity() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    module_function_arity::module(),
+                    module_function_arity::function(),
+                    (1_u8..=255_u8),
+                )
+                    .prop_map(|(module, function, arity)| {
+                        closure(&arc_process.clone(), module, function, arity)
+                    }),
+                |function| {
+                    let Ready {
+                        arc_process: child_arc_process,
+                        result,
+                    } = run_until_ready(
+                        Default::default(),
+                        |child_process| {
+                            let child_function = function.clone_to_process(child_process);
+                            let child_arguments = Term::NIL;
+
+                            place_frame_with_arguments(
+                                child_process,
+                                Placement::Push,
+                                child_function,
+                                child_arguments,
+                            )
+                        },
+                        5_000,
+                    )
+                    .unwrap();
+
+                    prop_assert_eq!(result, Err(badarity!(&arc_process, function, Term::NIL)));
+
+                    mem::drop(child_arc_process);
+
+                    Ok(())
+                },
+            )
+            .unwrap();
+    });
+}
+
+#[test]
+fn with_arity_returns_function_return() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    module_function_arity::module(),
+                    module_function_arity::function(),
+                )
+                    .prop_map(|(module, function)| {
+                        let creator = arc_process.pid_term();
+                        let module_function_arity = Arc::new(ModuleFunctionArity {
+                            module,
+                            function,
+                            arity: 0,
+                        });
+                        let code: Code = |arc_process: &Arc<Process>| {
+                            arc_process.return_from_call(atom_unchecked("return_from_fn"))?;
+
+                            Process::call_code(arc_process)
+                        };
+
+                        arc_process
+                            .closure_with_env_from_slice(module_function_arity, code, creator, &[])
+                            .unwrap()
+                    }),
+                |function| {
+                    let Ready {
+                        arc_process: child_arc_process,
+                        result,
+                    } = run_until_ready(
+                        Default::default(),
+                        |child_process| {
+                            let child_function = function.clone_to_process(child_process);
+                            let child_arguments = Term::NIL;
+
+                            place_frame_with_arguments(
+                                child_process,
+                                Placement::Push,
+                                child_function,
+                                child_arguments,
+                            )
+                        },
+                        5_000,
+                    )
+                    .unwrap();
+
+                    prop_assert_eq!(result, Ok(atom_unchecked("return_from_fn")));
+
+                    mem::drop(child_arc_process);
+
+                    Ok(())
+                },
+            )
+            .unwrap();
+    });
+}

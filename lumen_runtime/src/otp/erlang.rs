da59b5f834e1410d1347eb21618b463a7b822ea1
Luke Imhoff <Kronic.Deth@gmail.com>
2019-09-26 14:48:56 -0500

    erase/1

--- lumen_runtime/src/otp/erlang.rs
+++ lumen_runtime/src/otp/erlang.rs
@@ -45,6 +45,7 @@ pub mod div_2;
 pub mod divide_2;
 pub mod element_2;
 pub mod erase_0;
+pub mod erase_1;
 pub mod error_1;
 pub mod error_2;
 pub mod exit_1;
---
+++ lumen_runtime/src/otp/erlang/erase_1/test.rs
@@ -0,0 +1,46 @@
+use proptest::prop_assert_eq;
+use proptest::test_runner::{Config, TestRunner};
+
+use liblumen_alloc::erts::term::{atom_unchecked, Term};
+
+use crate::otp::erlang::erase_1::native;
+use crate::scheduler::with_process_arc;
+use crate::test::strategy;
+
+#[test]
+fn without_key_returns_undefined() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(&strategy::term(arc_process.clone()), |key| {
+                prop_assert_eq!(native(&arc_process, key), atom_unchecked("undefined"));
+
+                Ok(())
+            })
+            .unwrap();
+    });
+}
+
+#[test]
+fn with_key_returns_value_and_removes_key_from_dictionary() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    strategy::term(arc_process.clone()),
+                    strategy::term(arc_process.clone()),
+                ),
+                |(key, value)| {
+                    arc_process.put(key, value).unwrap();
+
+                    prop_assert_eq!(arc_process.get(key), value);
+
+                    prop_assert_eq!(native(&arc_process, key), value);
+
+                    prop_assert_eq!(arc_process.get(key), Term::NIL);
+
+                    Ok(())
+                },
+            )
+            .unwrap();
+    });
+}

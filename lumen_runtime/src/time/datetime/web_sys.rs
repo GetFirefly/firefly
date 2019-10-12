c037a93451d42e1cb811de8a859c803c13906624
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-10-12 07:37:05 -0500

    Add 1 to the month returned by get_utc_month since it is zero-based

--- lumen_runtime/src/time/datetime/web_sys.rs
+++ lumen_runtime/src/time/datetime/web_sys.rs
@@ -5,7 +5,7 @@ pub fn get_utc_now() -> [usize; 6] {
 
     [
         now.get_utc_full_year() as usize,
-        now.get_utc_month() as usize,
+        (now.get_utc_month() as usize) + 1, // Since months in javascript are 0-based
         now.get_utc_date() as usize,
         now.get_utc_hours() as usize,
         now.get_utc_minutes() as usize,

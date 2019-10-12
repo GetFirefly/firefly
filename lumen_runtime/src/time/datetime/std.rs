86bed210a1da5837b391a74a56b14902c65f60f6
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-10-12 14:59:59 -0500

    Implement localtime/0

--- lumen_runtime/src/time/datetime/std.rs
+++ lumen_runtime/src/time/datetime/std.rs
@@ -1,14 +1,20 @@
 use chrono::prelude::*;
 
-pub fn get_utc_now() -> [usize; 6] {
-    let utc: DateTime<Utc> = Utc::now();
+pub fn get_local_now() -> [usize; 6] {
+    datetime_to_array(Local::now())
+}
 
+pub fn get_utc_now() -> [usize; 6] {
+    datetime_to_array(Utc::now())
+}
+
+fn datetime_to_array<Tz: TimeZone>(datetime: DateTime<Tz>) -> [usize; 6] {
     [
-        utc.year() as usize,
-        utc.month() as usize,
-        utc.day() as usize,
-        utc.hour() as usize,
-        utc.minute() as usize,
-        utc.second() as usize,
+        datetime.year() as usize,
+        datetime.month() as usize,
+        datetime.day() as usize,
+        datetime.hour() as usize,
+        datetime.minute() as usize,
+        datetime.second() as usize,
     ]
 }

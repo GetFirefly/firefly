bfd9dad4c3e71523ca4f916bcf92e622caafa4fb
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-10-13 15:21:09 -0500

    Made sure date_0 test reflects its name

--- lumen_runtime/src/otp/erlang/date_0/test.rs
+++ lumen_runtime/src/otp/erlang/date_0/test.rs
@@ -1,10 +1,12 @@
-use crate::otp::erlang::date_0::native;
+use crate::otp::erlang::date_0;
+use crate::otp::erlang::tuple_size_1;
 use crate::scheduler::with_process;
 
 #[test]
 fn returns_date_as_a_three_tuple() {
     with_process(|process| {
-        let result = native(process);
-        assert!(result.is_ok());
+        let date_result = date_0::native(process).unwrap();
+        let tuple_size_result = tuple_size_1::native(process, date_result).unwrap();
+        assert!(tuple_size_result == process.integer(3).unwrap());
     });
 }

a274415fd0db7b7d03e8da75f727c81f7c58da13
Luke Imhoff <Kronic.Deth@gmail.com>
2019-10-15 17:47:47 -0500

    spawn_opt/2

---
+++ lumen_runtime/src/otp/erlang/spawn_opt_2/test/with_function/with_empty_list_options.rs
@@ -0,0 +1,63 @@
+mod with_arity_zero;
+
+use super::*;
+
+use proptest::strategy::Strategy;
+
+use liblumen_alloc::erts::exception::Exception;
+
+#[test]
+fn without_arity_zero_returns_pid_to_parent_and_child_process_exits_badarity() {
+    with_process_arc(|arc_process| {
+        TestRunner::new(Config::with_source_file(file!()))
+            .run(
+                &(
+                    strategy::module_function_arity::module(),
+                    strategy::module_function_arity::function(),
+                    (1_u8..=255_u8),
+                )
+                    .prop_map(|(module, function, arity)| {
+                        strategy::term::closure(&arc_process, module, function, arity)
+                    }),
+                |function| {
+                    let result = native(&arc_process, function, OPTIONS);
+
+                    prop_assert!(result.is_ok());
+
+                    let child_pid_term = result.unwrap();
+
+                    prop_assert!(child_pid_term.is_pid());
+
+                    let child_pid: Pid = child_pid_term.try_into().unwrap();
+
+                    let child_arc_process = pid_to_process(&child_pid).unwrap();
+
+                    let scheduler = Scheduler::current();
+
+                    prop_assert!(scheduler.run_once());
+                    prop_assert!(scheduler.run_once());
+                    prop_assert!(scheduler.run_once());
+
+                    match *child_arc_process.status.read() {
+                        Status::Exiting(ref exception) => {
+                            prop_assert_eq!(
+                                Exception::Runtime(*exception),
+                                badarity!(&child_arc_process, function, Term::NIL)
+                            );
+                        }
+                        ref status => {
+                            return Err(proptest::test_runner::TestCaseError::fail(format!(
+                                "Child process did not exit.  Status is {:?}",
+                                status
+                            )))
+                        }
+                    }
+
+                    Ok(())
+                },
+            )
+            .unwrap();
+    });
+}
+
+const OPTIONS: Term = Term::NIL;

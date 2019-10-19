4bf9591288da40bcc833874015bf410362ab665e
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-10-19 09:12:44 -0500

    Use duration since epoch instead of elasped

--- lumen_runtime/src/time/system/std.rs
+++ lumen_runtime/src/time/system/std.rs
@@ -3,5 +3,8 @@ use std::time::SystemTime;
 use super::Milliseconds;
 
 pub fn time_in_milliseconds() -> Milliseconds {
-    SystemTime::now().elapsed().unwrap().as_millis() as Milliseconds
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as Milliseconds
 }

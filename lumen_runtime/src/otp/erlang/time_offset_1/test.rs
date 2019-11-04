f31536f02212c400f9c2e2568a8f15abbb4038d0
Luke Imhoff <Kronic.Deth@gmail.com>
2019-11-04 16:10:44 -0600

    Increase TIME_OFFSET_DELTA_LIMIT from 5 to 20
    The increase from 2 to 5 is still not enough on CI

--- lumen_runtime/src/otp/erlang/time_offset_1/test.rs
+++ lumen_runtime/src/otp/erlang/time_offset_1/test.rs
@@ -1,116 +1,56 @@
-use crate::otp::erlang::monotonic_time_1;
-use crate::otp::erlang::subtract_2;
-use crate::otp::erlang::system_time_1;
-use crate::otp::erlang::time_offset_1;
-use crate::scheduler::with_process;
 use liblumen_alloc::erts::term::atom_unchecked;
 
-const TIME_OFFSET_DELTA_LIMIT: u64 = 5;
+use crate::otp::erlang::{monotonic_time_1, subtract_2, system_time_1, time_offset_1};
+use crate::scheduler::with_process;
+
+const TIME_OFFSET_DELTA_LIMIT: u64 = 20;
 
 #[test]
 fn approximately_system_time_minus_monotonic_time_in_seconds() {
-    with_process(|process| {
-        let unit = atom_unchecked("second");
-
-        let monotonic_time = monotonic_time_1::native(process, unit).unwrap();
-        let system_time = system_time_1::native(process, unit).unwrap();
-        let time_offset = time_offset_1::native(process, unit).unwrap();
-        let expected_time_offset =
-            subtract_2::native(process, system_time, monotonic_time).unwrap();
-
-        let time_offset_delta =
-            subtract_2::native(process, expected_time_offset, time_offset).unwrap();
-
-        assert!(time_offset_delta <= process.integer(TIME_OFFSET_DELTA_LIMIT).unwrap());
-    });
+    approximately_system_time_minus_monotonic_time_in_unit("second")
 }
 
 #[test]
 fn approximately_system_time_minus_monotonic_time_in_milliseconds() {
-    with_process(|process| {
-        let unit = atom_unchecked("millisecond");
-
-        let monotonic_time = monotonic_time_1::native(process, unit).unwrap();
-        let system_time = system_time_1::native(process, unit).unwrap();
-        let time_offset = time_offset_1::native(process, unit).unwrap();
-        let expected_time_offset =
-            subtract_2::native(process, system_time, monotonic_time).unwrap();
-
-        let time_offset_delta =
-            subtract_2::native(process, expected_time_offset, time_offset).unwrap();
-
-        assert!(time_offset_delta <= process.integer(TIME_OFFSET_DELTA_LIMIT).unwrap());
-    });
+    approximately_system_time_minus_monotonic_time_in_unit("millisecond")
 }
 
 #[test]
 fn approximately_system_time_minus_monotonic_time_in_microseconds() {
-    with_process(|process| {
-        let unit = atom_unchecked("microsecond");
-
-        let monotonic_time = monotonic_time_1::native(process, unit).unwrap();
-        let system_time = system_time_1::native(process, unit).unwrap();
-        let time_offset = time_offset_1::native(process, unit).unwrap();
-        let expected_time_offset =
-            subtract_2::native(process, system_time, monotonic_time).unwrap();
-
-        let time_offset_delta =
-            subtract_2::native(process, expected_time_offset, time_offset).unwrap();
-
-        assert!(time_offset_delta <= process.integer(TIME_OFFSET_DELTA_LIMIT).unwrap());
-    });
+    approximately_system_time_minus_monotonic_time_in_unit("microsecond")
 }
 
 #[test]
 fn approximately_system_time_minus_monotonic_time_in_nanoseconds() {
-    with_process(|process| {
-        let unit = atom_unchecked("nanosecond");
-
-        let monotonic_time = monotonic_time_1::native(process, unit).unwrap();
-        let system_time = system_time_1::native(process, unit).unwrap();
-        let time_offset = time_offset_1::native(process, unit).unwrap();
-        let expected_time_offset =
-            subtract_2::native(process, system_time, monotonic_time).unwrap();
-
-        let time_offset_delta =
-            subtract_2::native(process, expected_time_offset, time_offset).unwrap();
-
-        assert!(time_offset_delta <= process.integer(TIME_OFFSET_DELTA_LIMIT).unwrap());
-    });
+    approximately_system_time_minus_monotonic_time_in_unit("nanosecond");
 }
 
 #[test]
 fn approximately_system_time_minus_monotonic_time_in_native_time_units() {
-    with_process(|process| {
-        let unit = atom_unchecked("native");
-
-        let monotonic_time = monotonic_time_1::native(process, unit).unwrap();
-        let system_time = system_time_1::native(process, unit).unwrap();
-        let time_offset = time_offset_1::native(process, unit).unwrap();
-        let expected_time_offset =
-            subtract_2::native(process, system_time, monotonic_time).unwrap();
-
-        let time_offset_delta =
-            subtract_2::native(process, expected_time_offset, time_offset).unwrap();
-
-        assert!(time_offset_delta <= process.integer(TIME_OFFSET_DELTA_LIMIT).unwrap());
-    });
+    approximately_system_time_minus_monotonic_time_in_unit("native");
 }
 
 #[test]
 fn approximately_system_time_minus_monotonic_time_in_perf_counter_ticks() {
-    with_process(|process| {
-        let unit = atom_unchecked("perf_counter");
+    approximately_system_time_minus_monotonic_time_in_unit("perf_counter");
+}
 
+fn approximately_system_time_minus_monotonic_time_in_unit(unit_str: &str) {
+    with_process(|process| {
+        let unit = atom_unchecked(unit_str);
         let monotonic_time = monotonic_time_1::native(process, unit).unwrap();
         let system_time = system_time_1::native(process, unit).unwrap();
         let time_offset = time_offset_1::native(process, unit).unwrap();
         let expected_time_offset =
             subtract_2::native(process, system_time, monotonic_time).unwrap();
-
         let time_offset_delta =
             subtract_2::native(process, expected_time_offset, time_offset).unwrap();
 
-        assert!(time_offset_delta <= process.integer(TIME_OFFSET_DELTA_LIMIT).unwrap());
+        assert!(
+            time_offset_delta <= process.integer(TIME_OFFSET_DELTA_LIMIT).unwrap(),
+            "time_offset_delta ({:?}) <= TIME_OFFSET_DELTA_LIMIT ({:?})",
+            time_offset_delta,
+            TIME_OFFSET_DELTA_LIMIT
+        );
     });
 }

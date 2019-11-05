077ce963d5cadf5a019c2a0aa2b5e99260c6b1d1
Luke Imhoff <Kronic.Deth@gmail.com>
2019-11-05 08:51:43 -0600

    Lumen.Web.Node.replace_child/3 native_implemented_function

--- lumen_web/src/node/replace_child_3.rs
+++ lumen_web/src/node/replace_child_3.rs
@@ -1,83 +1,31 @@
-use std::sync::Arc;
+//! Unlike [Node.appendChild](https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild),
+//! this does not return the appended child as that is prone to errors with chaining.
+//!
+//! ```elixir
+//! case Lumen.Web.Node.replace_child(parent, new_child, old_child) do
+//!   {:ok, replaced_child} -> ...
+//!   {:error, :hierarchy_request} -> ...
+//!   {:error, :wrong_document} -> ...
+//!   {:error, :no_modification_allowed} -> ...
+//!   {:error, :not_found} -> ...
+//!   {:error, :not_supported} -> ...
+//! end
+//! ```
 
 use wasm_bindgen::JsCast;
 
 use web_sys::DomException;
 
 use liblumen_alloc::erts::exception;
-use liblumen_alloc::erts::exception::system::Alloc;
-use liblumen_alloc::erts::process::code::stack::frame::{Frame, Placement};
-use liblumen_alloc::erts::process::code::{self, result_from_exception};
 use liblumen_alloc::erts::process::Process;
-use liblumen_alloc::erts::term::{atom_unchecked, Atom, Term};
-use liblumen_alloc::erts::ModuleFunctionArity;
+use liblumen_alloc::erts::term::{atom_unchecked, Term};
+
+use lumen_runtime_macros::native_implemented_function;
 
 use crate::node::node_from_term;
 use crate::{error, ok_tuple};
 
-/// Unlike [Node.appendChild](https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild),
-/// this does not return the appended child as that is prone to errors with chaining.
-///
-/// ```elixir
-/// case Lumen.Web.Node.replace_child(parent, new_child, old_child) do
-///   {:ok, replaced_child} -> ...
-///   {:error, :hierarchy_request} -> ...
-///   {:error, :wrong_document} -> ...
-///   {:error, :no_modification_allowed} -> ...
-///   {:error, :not_found} -> ...
-///   {:error, :not_supported} -> ...
-/// end
-/// ```
-pub fn place_frame_with_arguments(
-    process: &Process,
-    placement: Placement,
-    parent: Term,
-    new_child: Term,
-    old_child: Term,
-) -> Result<(), Alloc> {
-    process.stack_push(old_child)?;
-    process.stack_push(new_child)?;
-    process.stack_push(parent)?;
-    process.place_frame(frame(), placement);
-
-    Ok(())
-}
-
-// Private
-
-pub fn code(arc_process: &Arc<Process>) -> code::Result {
-    arc_process.reduce();
-
-    let parent = arc_process.stack_pop().unwrap();
-    let new_child = arc_process.stack_pop().unwrap();
-    let old_child = arc_process.stack_pop().unwrap();
-
-    match native(arc_process, parent, new_child, old_child) {
-        Ok(ok_or_error_tuple) => {
-            arc_process.return_from_call(ok_or_error_tuple)?;
-
-            Process::call_code(arc_process)
-        }
-        Err(exception) => result_from_exception(arc_process, exception),
-    }
-}
-
-fn frame() -> Frame {
-    Frame::new(module_function_arity(), code)
-}
-
-fn function() -> Atom {
-    Atom::try_from_str("replace_child").unwrap()
-}
-
-fn module_function_arity() -> Arc<ModuleFunctionArity> {
-    Arc::new(ModuleFunctionArity {
-        module: super::module(),
-        function: function(),
-        arity: 3,
-    })
-}
-
+#[native_implemented_function(replace_child/3)]
 fn native(process: &Process, parent: Term, old_child: Term, new_child: Term) -> exception::Result {
     let parent_node = node_from_term(parent)?;
     let old_child_node = node_from_term(old_child)?;

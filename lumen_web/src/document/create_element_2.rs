dd66b5a7ae0de8cb49ec0adf516b3bbc5f0f25a4
Luke Imhoff <Kronic.Deth@gmail.com>
2019-11-05 07:53:08 -0600

    Lumen.Web.Document.create_element/2 native_implemented_function

--- lumen_web/src/document/create_element_2.rs
+++ lumen_web/src/document/create_element_2.rs
@@ -1,71 +1,22 @@
-use std::sync::Arc;
+//! ```elixir
+//! case Lumen.Web.Document.create_element(document, tag) do
+//!   {:ok, element} -> ...
+//!   {:error, {:tag, tag}} -> ...
+//! end
+//! ```
 
 use liblumen_alloc::erts::exception;
-use liblumen_alloc::erts::exception::system::Alloc;
-use liblumen_alloc::erts::process::code::stack::frame::{Frame, Placement};
-use liblumen_alloc::erts::process::code::{self, result_from_exception};
 use liblumen_alloc::erts::process::Process;
-use liblumen_alloc::erts::term::{atom_unchecked, Atom, Term};
-use liblumen_alloc::erts::ModuleFunctionArity;
+use liblumen_alloc::erts::term::{atom_unchecked, Term};
 
 use lumen_runtime::binary_to_string::binary_to_string;
 
 use crate::document::document_from_term;
 use crate::{error, ok_tuple};
 
-/// ```elixir
-/// case Lumen.Web.Document.create_element(document, tag) do
-///   {:ok, element} -> ...
-///   {:error, {:tag, tag}} -> ...
-/// end
-/// ```
-pub fn place_frame_with_arguments(
-    process: &Process,
-    placement: Placement,
-    document: Term,
-    tag: Term,
-) -> Result<(), Alloc> {
-    process.stack_push(tag)?;
-    process.stack_push(document)?;
-    process.place_frame(frame(), placement);
+use lumen_runtime_macros::native_implemented_function;
 
-    Ok(())
-}
-
-// Private
-
-fn code(arc_process: &Arc<Process>) -> code::Result {
-    arc_process.reduce();
-
-    let document = arc_process.stack_pop().unwrap();
-    let tag = arc_process.stack_pop().unwrap();
-
-    match native(arc_process, document, tag) {
-        Ok(ok_or_error) => {
-            arc_process.return_from_call(ok_or_error)?;
-
-            Process::call_code(arc_process)
-        }
-        Err(exception) => result_from_exception(arc_process, exception),
-    }
-}
-
-fn frame() -> Frame {
-    Frame::new(module_function_arity(), code)
-}
-
-fn function() -> Atom {
-    Atom::try_from_str("create_element").unwrap()
-}
-
-fn module_function_arity() -> Arc<ModuleFunctionArity> {
-    Arc::new(ModuleFunctionArity {
-        module: super::module(),
-        function: function(),
-        arity: 2,
-    })
-}
-
+#[native_implemented_function(create_element/2)]
 pub fn native(process: &Process, document: Term, tag: Term) -> exception::Result {
     let document_document = document_from_term(document)?;
     let tag_string: String = binary_to_string(tag)?;

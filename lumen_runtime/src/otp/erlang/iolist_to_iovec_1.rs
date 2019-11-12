d93ab1da953d07cde9f5f7f60f37c6dee653cae3
Mike Wilkerson <mwilkerson@gmail.com>
2019-11-12 10:43:35 -0800

    Revise iolist_to_iovec/1 to process mixed iolists

--- lumen_runtime/src/otp/erlang/iolist_to_iovec_1.rs
+++ lumen_runtime/src/otp/erlang/iolist_to_iovec_1.rs
@@ -5,9 +5,10 @@
 #[cfg(all(not(target_arch = "wasm32"), test))]
 mod test;
 
+use liblumen_alloc::badarg;
 use liblumen_alloc::erts::exception;
 use liblumen_alloc::erts::process::Process;
-use liblumen_alloc::erts::term::Term;
+use liblumen_alloc::erts::term::{Term, TypedTerm};
 use lumen_runtime_macros::native_implemented_function;
 
 use crate::otp;
@@ -15,21 +16,28 @@ use crate::otp;
 /// Returns a binary that is made from the integers and binaries given in iolist
 #[native_implemented_function(iolist_to_iovec/1)]
 pub fn native(process: &Process, iolist_or_binary: Term) -> exception::Result {
-    let mut iovec: Vec<Term> = Vec::new();
+    let iovec: Vec<Term> =
+      if iolist_or_binary.is_binary() {
+        vec![iolist_or_binary]
+      } else {
+        match iolist_or_binary.to_typed_term().unwrap() {
+            TypedTerm::List(boxed_cons) => {
+              boxed_cons
+              .into_iter()
+              .map(|item| { 
+                let term: Term = item.unwrap();
 
-    if iolist_or_binary.is_binary() {
-        iovec.push(iolist_or_binary);
-    } else {
-        let mut stack: Vec<Term> = vec![iolist_or_binary];
-
-        while let Some(top) = stack.pop() {
-            if top.is_binary() {
-                iovec.push(top);
-            } else {
-                iovec.push(otp::erlang::list_to_binary_1::native(process, top).unwrap());
-            }
+                if term.is_binary() {
+                  term
+                } else {
+                  otp::erlang::list_to_binary_1::native(process,term).unwrap()
+                }
+              })
+              .collect()
+            },
+            _ => return Err(badarg!().into()),
         }
-    }
+      };
 
     Ok(process.list_from_slice(&iovec).unwrap())
 }
--- lumen_runtime/src/otp/erlang/iolist_to_iovec_1/test.rs
+++ lumen_runtime/src/otp/erlang/iolist_to_iovec_1/test.rs
@@ -14,3 +14,59 @@ fn with_binary_returns_binary_in_list() {
         )
     });
 }
+
+#[test]
+fn with_mixed_iolists_returns_iovec() {
+    with_process(|process| {
+      let bin1 = process.binary_from_bytes(&[57, 58, 59]).unwrap();
+      let bin2 = process.binary_from_bytes(&[60, 61]).unwrap();
+      let bin3 = process.binary_from_bytes(&[6]).unwrap();
+
+      let iolist1 = process
+          .improper_list_from_slice(
+              &[
+                  bin1,
+                  process.integer(77).unwrap(),
+                  process
+                      .list_from_slice(&[
+                          process.integer(9).unwrap(),
+                          process.integer(8).unwrap(),
+                          bin2,
+                      ])
+                      .unwrap(),
+                  process.integer(7).unwrap(),
+              ],
+              bin3,
+          )
+          .unwrap();
+
+      let iolist2 = process.
+          list_from_slice(
+            &[
+              process.integer(42).unwrap(),
+              process.integer(43).unwrap()
+            ]
+          ).unwrap();
+
+      let iolist_arg = process.list_from_slice(
+        &[
+          iolist1,
+          bin2,
+          iolist2
+        ]
+      ).unwrap();
+
+      assert_eq!(
+          otp::erlang::iolist_to_iovec_1::native(process, iolist_arg),
+          Ok(process
+              .list_from_slice(
+                &[
+                  process.binary_from_bytes(&[57, 58, 59, 77, 9, 8, 60, 61, 7, 6]).unwrap(),
+                  process.binary_from_bytes(&[60, 61]).unwrap(),
+                  process.binary_from_bytes(&[42, 43]).unwrap()
+                ]
+              )
+              .unwrap())
+      )
+  });
+}

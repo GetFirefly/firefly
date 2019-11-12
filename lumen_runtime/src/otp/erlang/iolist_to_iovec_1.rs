227ae257544ee136478b339510e26e9c7eba10c1
Mike Wilkerson <mwilkerson@gmail.com>
2019-11-12 07:45:14 -0800

    WIP: initial implementation of iolist_to_iovec_1

---
+++ lumen_runtime/src/otp/erlang/iolist_to_iovec_1.rs
@@ -0,0 +1,40 @@
+// wasm32 proptest cannot be compiled at the same time as non-wasm32 proptest, so disable tests that
+// use proptest completely for wasm32
+//
+// See https://github.com/rust-lang/cargo/issues/4866
+#[cfg(all(not(target_arch = "wasm32"), test))]
+mod test;
+
+use liblumen_alloc::erts::exception;
+use liblumen_alloc::erts::process::Process;
+use liblumen_alloc::erts::term::Term;
+use lumen_runtime_macros::native_implemented_function;
+
+use crate::otp;
+
+/// Returns a binary that is made from the integers and binaries given in iolist
+#[native_implemented_function(iolist_to_iovec/1)]
+pub fn native(process: &Process, iolist_or_binary: Term) -> exception::Result {
+  let mut iovec: Vec<Term> = Vec::new();
+
+  if iolist_or_binary.is_binary() {
+    iovec.push(iolist_or_binary);
+  } else {
+    let mut stack: Vec<Term> = vec![iolist_or_binary];
+
+    while let Some(top) = stack.pop() {
+      if top.is_binary() {
+        iovec.push(top);
+      } else {
+        iovec.push(
+          otp::erlang::list_to_binary_1::native(
+            process,
+            top
+          ).unwrap()
+        );
+      }
+    }
+  }
+
+  Ok(process.list_from_slice(&iovec).unwrap())
+}
---
+++ lumen_runtime/src/otp/erlang/iolist_to_iovec_1/test.rs
@@ -0,0 +1,18 @@
+use crate::otp;
+use crate::scheduler::with_process;
+
+#[test]
+fn with_binary_returns_binary_in_list() {
+    with_process(|process| {
+        let bin = process.binary_from_bytes(&[1, 2, 3]).unwrap();
+
+        assert_eq!(
+          otp::erlang::iolist_to_iovec_1::native(process, bin),
+          Ok(
+            process.list_from_slice(&[
+              process.binary_from_bytes(&[1, 2, 3]).unwrap()
+            ]).unwrap()
+          )
+        )
+    });
+}

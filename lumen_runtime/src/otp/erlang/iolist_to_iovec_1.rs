cdc8d7f810034cb96096277a09721f955b0aedaf
Mike Wilkerson <mwilkerson@gmail.com>
2019-11-12 10:43:56 -0800

    auto-formatting

--- lumen_runtime/src/otp/erlang/iolist_to_iovec_1.rs
+++ lumen_runtime/src/otp/erlang/iolist_to_iovec_1.rs
@@ -16,28 +16,25 @@ use crate::otp;
 /// Returns a binary that is made from the integers and binaries given in iolist
 #[native_implemented_function(iolist_to_iovec/1)]
 pub fn native(process: &Process, iolist_or_binary: Term) -> exception::Result {
-    let iovec: Vec<Term> =
-      if iolist_or_binary.is_binary() {
+    let iovec: Vec<Term> = if iolist_or_binary.is_binary() {
         vec![iolist_or_binary]
-      } else {
+    } else {
         match iolist_or_binary.to_typed_term().unwrap() {
-            TypedTerm::List(boxed_cons) => {
-              boxed_cons
-              .into_iter()
-              .map(|item| { 
-                let term: Term = item.unwrap();
+            TypedTerm::List(boxed_cons) => boxed_cons
+                .into_iter()
+                .map(|item| {
+                    let term: Term = item.unwrap();
 
-                if term.is_binary() {
-                  term
-                } else {
-                  otp::erlang::list_to_binary_1::native(process,term).unwrap()
-                }
-              })
-              .collect()
-            },
+                    if term.is_binary() {
+                        term
+                    } else {
+                        otp::erlang::list_to_binary_1::native(process, term).unwrap()
+                    }
+                })
+                .collect(),
             _ => return Err(badarg!().into()),
         }
-      };
+    };
 
     Ok(process.list_from_slice(&iovec).unwrap())
 }

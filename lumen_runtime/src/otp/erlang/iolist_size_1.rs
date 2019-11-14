fb9b7c573621acea5141d000fc7f77984f8676e8
Mike Wilkerson <mwilkerson@gmail.com>
2019-11-14 11:31:13 -0800

    auto-formatting

--- lumen_runtime/src/otp/erlang/iolist_size_1.rs
+++ lumen_runtime/src/otp/erlang/iolist_size_1.rs
@@ -7,51 +7,51 @@ mod test;
 
 use liblumen_alloc::badarg;
 use liblumen_alloc::erts::exception;
+use liblumen_alloc::erts::exception::Exception;
 use liblumen_alloc::erts::process::Process;
+use liblumen_alloc::erts::term::binary::Bitstring;
 use liblumen_alloc::erts::term::{Term, TypedTerm};
 use lumen_runtime_macros::native_implemented_function;
-use liblumen_alloc::erts::exception::Exception;
-use liblumen_alloc::erts::term::binary::Bitstring;
 
 /// Returns the size, in bytes, of the binary that would be result from iolist_to_binary/1
 #[native_implemented_function(iolist_size/1)]
 pub fn native(process: &Process, iolist_or_binary: Term) -> exception::Result {
-  let mut stack: Vec<Term> = vec![iolist_or_binary];
-  Ok(process.integer(size(process, &mut stack, 0).unwrap())?)
+    let mut stack: Vec<Term> = vec![iolist_or_binary];
+    Ok(process.integer(size(process, &mut stack, 0).unwrap())?)
 }
 
 fn size(process: &Process, vec: &mut Vec<Term>, acc: usize) -> Result<usize, Exception> {
-  if let Some(term) = vec.pop() {
-    match term.to_typed_term().unwrap() {
+    if let Some(term) = vec.pop() {
+        match term.to_typed_term().unwrap() {
+            TypedTerm::SmallInteger(_) => size(process, vec, acc + 1),
 
-      TypedTerm::SmallInteger(_) => 
-        size(process, vec, acc + 1),
+            TypedTerm::Nil => size(process, vec, acc),
 
-      TypedTerm::Nil => 
-        size(process, vec, acc),
+            TypedTerm::Boxed(boxed) => match boxed.to_typed_term().unwrap() {
+                TypedTerm::HeapBinary(heap_binary) => {
+                    size(process, vec, acc + heap_binary.full_byte_len())
+                }
 
-      TypedTerm::Boxed(boxed) => match boxed.to_typed_term().unwrap() {
-        TypedTerm::HeapBinary(heap_binary) =>
-          size(process, vec, acc + heap_binary.full_byte_len()),
+                TypedTerm::ProcBin(proc_binary) => {
+                    size(process, vec, acc + proc_binary.full_byte_len())
+                }
 
-        TypedTerm::ProcBin(proc_binary) =>
-          size(process, vec, acc + proc_binary.full_byte_len()),
+                TypedTerm::SubBinary(subbinary) => {
+                    size(process, vec, acc + subbinary.full_byte_len())
+                }
 
-        TypedTerm::SubBinary(subbinary) =>
-          size(process, vec, acc + subbinary.full_byte_len()),
+                _ => Err(badarg!().into()),
+            },
 
-        _ => Err(badarg!().into())
-      },
+            TypedTerm::List(boxed_cons) => {
+                vec.push(boxed_cons.tail);
+                vec.push(boxed_cons.head);
+                size(process, vec, acc)
+            }
 
-      TypedTerm::List(boxed_cons) => {
-        vec.push(boxed_cons.tail);
-        vec.push(boxed_cons.head);
-        size(process, vec, acc)
-      },
-
-      _ => Err(badarg!().into())
+            _ => Err(badarg!().into()),
+        }
+    } else {
+        Ok(acc)
     }
-  } else {
-    Ok(acc)
-  }
 }

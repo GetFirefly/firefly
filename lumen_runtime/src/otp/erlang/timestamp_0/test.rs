a061a06dc13eddc9150ddcd9ed69dddc2ac574e5
Bryan Joseph <bryanjos@users.noreply.github.com>
2019-11-18 19:54:58 -0600

    Implement 'approximately_system_time' test

--- lumen_runtime/src/otp/erlang/timestamp_0/test.rs
+++ lumen_runtime/src/otp/erlang/timestamp_0/test.rs
@@ -1,7 +1,9 @@
-use crate::otp::erlang::{system_time_1, timestamp_0, tuple_size_1};
+use crate::otp::erlang::{add_2, multiply_2, subtract_2, system_time_1, timestamp_0, tuple_size_1};
 use crate::scheduler::with_process;
 use liblumen_alloc::erts::term::prelude::*;
 use std::convert::TryInto;
+
+const DELTA_LIMIT_MICROSECONDS: u64 = 5;
 
 #[test]
 fn returns_a_three_element_tuple() {
@@ -16,6 +18,8 @@ fn returns_a_three_element_tuple() {
 
 #[test]
 fn approximately_system_time() {
+    // Take the result from timestamp and
+    // compare it to system time
     with_process(|process| {
         let unit = Atom::str_to_term("microsecond");
 
@@ -25,12 +29,32 @@ fn approximately_system_time() {
 
         let timestamp_tuple: Boxed<Tuple> = timestamp.try_into().unwrap();
 
-        let megasecs = timestamp_tuple.get_element(0).unwrap();
-        let secs = timestamp_tuple.get_element(1).unwrap();
+        let megasecs = multiply_2::native(
+            process,
+            timestamp_tuple.get_element(0).unwrap(),
+            process.integer(1000000000000 as usize).unwrap(),
+        )
+        .unwrap();
+        let secs = multiply_2::native(
+            process,
+            timestamp_tuple.get_element(1).unwrap(),
+            process.integer(1000000).unwrap(),
+        )
+        .unwrap();
+
         let microsecs = timestamp_tuple.get_element(2).unwrap();
 
-        println!("{:?} {:?} {:?}", megasecs, secs, microsecs);
+        let system_time_from_timestamp = add_2::native(
+            process,
+            add_2::native(process, megasecs, secs).unwrap(),
+            microsecs,
+        )
+        .unwrap();
 
-        assert!(process.integer(3).unwrap() == process.integer(3).unwrap());
+        let delta_limit_microseconds = process.integer(DELTA_LIMIT_MICROSECONDS).unwrap();
+
+        let delta = subtract_2::native(process, system_time_from_timestamp, system_time).unwrap();
+
+        assert!(delta < delta_limit_microseconds);
     });
 }

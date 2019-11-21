a89397b647db8c670b25a86a3c4a0fc289a44620
Luke Imhoff <Kronic.Deth@gmail.com>
2019-11-21 09:43:02 -0600

    Don't use badarg for timer read options

--- lumen_runtime/src/otp/erlang/read_timer_2.rs
+++ lumen_runtime/src/otp/erlang/read_timer_2.rs
@@ -7,6 +7,7 @@ mod test;
 
 use std::convert::TryInto;
 
+use liblumen_alloc::badarg;
 use liblumen_alloc::erts::exception;
 use liblumen_alloc::erts::process::Process;
 use liblumen_alloc::erts::term::prelude::Term;
@@ -18,7 +19,7 @@ use crate::timer;
 
 #[native_implemented_function(read_timer/2)]
 pub fn native(process: &Process, timer_reference: Term, options: Term) -> exception::Result<Term> {
-    let read_timer_options: timer::read::Options = options.try_into()?;
+    let read_timer_options: timer::read::Options = options.try_into().map_err(|_| badarg!())?;
 
     read_timer(timer_reference, read_timer_options, process)
 }

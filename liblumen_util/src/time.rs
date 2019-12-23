f26899a38c1866e5c08f02e0b06f688bc061be11
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2019-12-23 11:37:37 -0500

    Add HumanDuration to liblumen_util

--- liblumen_util/src/time.rs
+++ liblumen_util/src/time.rs
@@ -1,9 +1,62 @@
 use std::cell::Cell;
 use std::time::{Duration, Instant};
+use std::fmt;
 
 use crate::mem;
 
 thread_local!(static TIME_DEPTH: Cell<usize> = Cell::new(0));
+
+pub struct HumanDuration(Duration);
+impl HumanDuration {
+    pub fn since(i: Instant) -> Self {
+        Self(Instant::now().duration_since(i))
+    }
+}
+impl From<Duration> for HumanDuration {
+    fn from(d: Duration) -> Self {
+        Self(d)
+    }
+}
+impl fmt::Display for HumanDuration {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let t = self.0.as_secs();
+        let alt = f.alternate();
+        macro_rules! try_unit {
+            ($secs:expr, $sg:expr, $pl:expr, $s:expr) => {
+                let cnt = t / $secs;
+                if cnt == 1 {
+                    if alt {
+                        return write!(f, "{}{}", cnt, $s);
+                    } else {
+                        return write!(f, "{} {}", cnt, $sg);
+                    }
+                } else if cnt > 1 {
+                    if alt {
+                        return write!(f, "{}{}", cnt, $s);
+                    } else {
+                        return write!(f, "{} {}", cnt, $pl);
+                    }
+                }
+            };
+        }
+
+        if t > 0 {
+            try_unit!(365 * 24 * 60 * 60, "year", "years", "y");
+            try_unit!(7 * 24 * 60 * 60, "week", "weeks", "w");
+            try_unit!(24 * 60 * 60, "day", "days", "d");
+            try_unit!(60 * 60, "hour", "hours", "h");
+            try_unit!(60, "minute", "minutes", "m");
+            try_unit!(1, "second", "seconds", "s");
+        } else {
+            // Time was too precise for the standard path, use millis
+            let t = self.0.as_millis();
+            if t > 0 {
+                return write!(f, "{}{}", t, if alt { "ms" } else { " milliseconds" });
+            }
+        }
+        write!(f, "0{}", if alt { "s" } else { " seconds" })
+    }
+}
 
 pub fn time<T, F>(timing_enabled: bool, what: &str, f: F) -> T
 where

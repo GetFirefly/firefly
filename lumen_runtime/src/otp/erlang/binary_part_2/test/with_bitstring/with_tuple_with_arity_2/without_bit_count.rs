51a1578e48903112a1dcb61a039e31c1412c06ce
Luke Imhoff <Kronic.Deth@gmail.com>
2020-01-07 12:10:34 -0600

    test::proptest::with_positive_start_and_positive_length_returns_subbinary

--- lumen_runtime/src/otp/erlang/binary_part_2/test/with_bitstring/with_tuple_with_arity_2/without_bit_count.rs
+++ lumen_runtime/src/otp/erlang/binary_part_2/test/with_bitstring/with_tuple_with_arity_2/without_bit_count.rs
@@ -2,39 +2,8 @@ use super::*;
 
 #[test]
 fn with_positive_start_and_positive_length_returns_subbinary() {
-    run!(
-        |arc_process| {
-            (
-                Just(arc_process.clone()),
-                strategy::term::is_binary::with_byte_len_range((3..=6).into(), arc_process.clone()),
-            )
-                .prop_flat_map(|(arc_process, binary)| {
-                    let byte_len = total_byte_len(binary);
-
-                    // `start` must be 2 less than `byte_len` so that `length` can be at least 1
-                    (
-                        Just(arc_process.clone()),
-                        Just(binary),
-                        (1..=(byte_len - 2)),
-                    )
-                })
-                .prop_flat_map(|(arc_process, binary, start)| {
-                    (
-                        Just(arc_process.clone()),
-                        Just(binary),
-                        Just(start),
-                        1..=(total_byte_len(binary) - start),
-                    )
-                })
-                .prop_map(|(arc_process, binary, start, length)| {
-                    (
-                        arc_process.clone(),
-                        binary,
-                        arc_process.integer(start).unwrap(),
-                        arc_process.integer(length).unwrap(),
-                    )
-                })
-        },
+    crate::test::with_positive_start_and_positive_length_returns_subbinary(
+        file!(),
         returns_subbinary,
     );
 }

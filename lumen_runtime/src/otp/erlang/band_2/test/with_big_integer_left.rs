a6fef6c19a4016155861219ccdd3e28825646111
Luke Imhoff <Kronic.Deth@gmail.com>
2020-01-07 10:27:05 -0600

    band_2::test::with_integer_right_returns_bitwise_and

--- lumen_runtime/src/otp/erlang/band_2/test/with_big_integer_left.rs
+++ lumen_runtime/src/otp/erlang/band_2/test/with_big_integer_left.rs
@@ -2,26 +2,5 @@ use super::*;
 
 #[test]
 fn with_integer_right_returns_bitwise_and() {
-    run!(
-        |arc_process| {
-            (
-                Just(arc_process.clone()),
-                strategy::term::integer::big(arc_process.clone()),
-                strategy::term::is_integer(arc_process.clone()),
-            )
-        },
-        |(arc_process, left, right)| {
-            let result = native(&arc_process, left, right);
-
-            prop_assert!(result.is_ok());
-
-            let band = result.unwrap();
-
-            prop_assert!(band.is_integer());
-            prop_assert!(count_ones(band) <= count_ones(left));
-            prop_assert!(count_ones(band) <= count_ones(right));
-
-            Ok(())
-        },
-    );
+    super::with_integer_right_returns_bitwise_and(file!(), strategy::term::integer::big);
 }

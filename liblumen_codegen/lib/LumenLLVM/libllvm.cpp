9141e7be2ed9c958a8da99a0382ee41adc89ffd0
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2020-01-22 02:02:02 -0500

    Remove unused build code in libLumen

--- liblumen_codegen/lib/LumenLLVM/libllvm.cpp
+++
@@ -1,6 +0,0 @@
-//===----------------------------------------------------------------------===//
-//
-// This file is empty and serves only the purpose of making CMake happy because
-// you can't define a target with no sources.
-//
-//===----------------------------------------------------------------------===//

eed555854e099d5b7744bc84e302e43237f2af3b
Luke Imhoff <Kronic.Deth@gmail.com>
2020-06-10 14:51:40 -0500

    Remove test incompatble with run_through

--- runtimes/full/src/scheduler/test.rs
+++ runtimes/full/src/scheduler/test.rs
@@ -1,7 +1,5 @@
 mod erlang;
 
-use anyhow::*;
-
 use liblumen_alloc::erts::term::prelude::*;
 
 use crate::scheduler::{Scheduled, Scheduler};
@@ -25,25 +23,5 @@ fn scheduler_does_not_requeue_exiting_process() {
 
     assert!(scheduler.is_run_queued(&arc_process));
     assert!(scheduler::run_through(&arc_process));
-    assert!(!scheduler.is_run_queued(&arc_process));
-}
-
-#[test]
-fn scheduler_does_run_exiting_process() {
-    let arc_process = test::process::default();
-    let arc_dyn_scheduler = scheduler::current();
-    let scheduler = arc_dyn_scheduler
-        .as_any()
-        .downcast_ref::<Scheduler>()
-        .unwrap();
-
-    assert!(scheduler.is_run_queued(&arc_process));
-    assert!(scheduler::run_through(&arc_process));
-    assert!(scheduler.is_run_queued(&arc_process));
-
-    arc_process.exit_normal(anyhow!("Test").into());
-
-    assert!(scheduler.is_run_queued(&arc_process));
-    assert!(!scheduler::run_through(&arc_process));
     assert!(!scheduler.is_run_queued(&arc_process));
 }

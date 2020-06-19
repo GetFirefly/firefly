bd5afddf2be0d4155d578497020ad6c1fd912890
Luke Imhoff <Kronic.Deth@gmail.com>
2020-06-19 14:00:50 -0500

    Add null byte after each atom

--- compiler/codegen/src/generators/atom_table.rs
+++ compiler/codegen/src/generators/atom_table.rs
@@ -53,7 +53,7 @@ pub fn generate(
         let constant = builder.build_named_constant_string(
             &format!("__atom{}.value", id),
             s,
-            /* null_terminated= */ false,
+            /* null_terminated= */ true,
         );
         // The atom constants are not accessible directly, only via the table
         builder.set_linkage(constant, Linkage::Private);

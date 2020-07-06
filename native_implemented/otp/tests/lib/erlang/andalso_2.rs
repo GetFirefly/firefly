0273ffd6b129c0761c395ffda3199d1cf8dc9e05
Luke Imhoff <Kronic.Deth@gmail.com>
2020-07-05 21:00:03 -0500

    test_stdout! macro
    Remove duplication for Erlang tests.

--- native_implemented/otp/tests/lib/erlang/andalso_2.rs
+++ native_implemented/otp/tests/lib/erlang/andalso_2.rs
@@ -1,43 +1,4 @@
-use super::*;
-
 // `without_boolean_left_errors_badarg` in unit tests
 
-#[test]
-fn with_false_left_returns_false() {
-    let name = "with_false_left_returns_false";
-    let bin_path_buf = compile(file!(), name);
-
-    let output = Command::new(bin_path_buf)
-        .stdin(Stdio::null())
-        .output()
-        .unwrap();
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let stderr = String::from_utf8_lossy(&output.stderr);
-
-    assert_eq!(
-        stdout, ":'false'\n",
-        "\nstdout = {}\nstderr = {}",
-        stdout, stderr
-    );
-}
-
-#[test]
-fn with_true_left_returns_right() {
-    let name = "with_true_left_returns_right";
-    let bin_path_buf = compile(file!(), name);
-
-    let output = Command::new(bin_path_buf)
-        .stdin(Stdio::null())
-        .output()
-        .unwrap();
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let stderr = String::from_utf8_lossy(&output.stderr);
-
-    assert_eq!(
-        stdout, ":'right'\n",
-        "\nstdout = {}\nstderr = {}",
-        stdout, stderr
-    );
-}
+test_stdout!(with_false_left_returns_false, ":'false'\n");
+test_stdout!(with_true_left_returns_right, ":'right'\n");

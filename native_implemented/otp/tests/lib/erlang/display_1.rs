a1d36f6db0b23bb271f56b7172b1ae59839aee8d
Luke Imhoff <Kronic.Deth@gmail.com>
2020-08-03 14:43:21 -0500

    display([]) test

--- native_implemented/otp/tests/lib/erlang/display_1.rs
+++ native_implemented/otp/tests/lib/erlang/display_1.rs
@@ -1,5 +1,6 @@
 // `without_number_errors_badarg` in unit tests
 
+test_stdout!(with_empty_list, "[]\n");
 test_stdout!(with_atom, ":'atom'\n");
 test_stdout!(with_small_integer, "1\n0\n-1\n");
 test_stdout!(with_float, "1.2\n0.3\n-4.5\n");

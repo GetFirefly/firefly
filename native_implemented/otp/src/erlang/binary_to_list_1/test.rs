0d87c7184feaea3ea4094ec6fc6c9a4b12f74091
Luke Imhoff <Kronic.Deth@gmail.com>
2020-08-09 17:55:10 -0500

    Integration tests for binary_to_list/1

--- native_implemented/otp/src/erlang/binary_to_list_1/test.rs
+++ native_implemented/otp/src/erlang/binary_to_list_1/test.rs
@@ -1,7 +1,4 @@
-use proptest::prop_assert_eq;
-use proptest::strategy::{Just, Strategy};
-
-use liblumen_alloc::erts::term::prelude::Term;
+use proptest::strategy::Just;
 
 use crate::erlang::binary_to_list_1::result;
 use crate::test::strategy;
@@ -23,55 +20,4 @@ fn without_binary_errors_badarg() {
     );
 }
 
-#[test]
-fn with_binary_returns_list_of_bytes() {
-    run!(
-        |arc_process| {
-            (Just(arc_process.clone()), strategy::byte_vec()).prop_flat_map(
-                |(arc_process, byte_vec)| {
-                    (
-                        Just(arc_process.clone()),
-                        Just(byte_vec.clone()),
-                        strategy::term::binary::containing_bytes(byte_vec, arc_process.clone()),
-                    )
-                },
-            )
-        },
-        |(arc_process, byte_vec, binary)| {
-            // not using an iterator because that would too closely match the code under
-            // test
-            let list = match byte_vec.len() {
-                0 => Term::NIL,
-                1 => arc_process
-                    .cons(arc_process.integer(byte_vec[0]).unwrap(), Term::NIL)
-                    .unwrap(),
-                2 => arc_process
-                    .cons(
-                        arc_process.integer(byte_vec[0]).unwrap(),
-                        arc_process
-                            .cons(arc_process.integer(byte_vec[1]).unwrap(), Term::NIL)
-                            .unwrap(),
-                    )
-                    .unwrap(),
-                3 => arc_process
-                    .cons(
-                        arc_process.integer(byte_vec[0]).unwrap(),
-                        arc_process
-                            .cons(
-                                arc_process.integer(byte_vec[1]).unwrap(),
-                                arc_process
-                                    .cons(arc_process.integer(byte_vec[2]).unwrap(), Term::NIL)
-                                    .unwrap(),
-                            )
-                            .unwrap(),
-                    )
-                    .unwrap(),
-                len => unimplemented!("len = {:?}", len),
-            };
-
-            prop_assert_eq!(result(&arc_process, binary), Ok(list));
-
-            Ok(())
-        },
-    );
-}
+// `with_binary_returns_list_of_bytes` in integration tests

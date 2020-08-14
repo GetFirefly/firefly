0c3ad7ce8485d2c2654432fd5791e1e83b372277
Luke Imhoff <Kronic.Deth@gmail.com>
2020-08-13 20:58:33 -0500

    Integration tests for float_to_list/1

---
+++ native_implemented/otp/tests/lib/erlang/float_to_list_1/with_float.rs
@@ -0,0 +1,6 @@
+test_stdout!(returns_list, "\"-1.19999999999999995559e+00\"\n\"2.99999999999999988898e-01\"\n\"4.50000000000000000000e+00\"\n");
+test_stdout!(
+    is_the_same_as_float_to_list_2_with_scientific_20,
+    "\"0.00000000000000000000e+00\"\n\"1.00000000000000005551e-01\"\n"
+);
+// `is_dual_of_list_to_float_1` in unit tests
---
+++ native_implemented/otp/tests/lib/erlang/float_to_list_1/with_float/is_the_same_as_float_to_list_2_with_scientific_20/init.erl
@@ -0,0 +1,8 @@
+-module(init).
+-export([start/0]).
+-import(erlang, [display/1, float_to_list/1]).
+
+start() ->
+  display(float_to_list(0.0)),
+  display(float_to_list(0.1)).
+

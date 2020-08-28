ef90c7681a691e6d8e76f1558c79d186a1c8ccc1
Luke Imhoff <Kronic.Deth@gmail.com>
2020-08-28 13:00:39 -0500

    Check child exits before parent that will be killed by propagation

--- native_implemented/otp/tests/lib/erlang/process_flag_2/with_atom_flag/with_trap_exit_flag/with_true_value_then_false_value_exits_when_linked_process_does_not_exit_normal/init.erl
+++ native_implemented/otp/tests/lib/erlang/process_flag_2/with_atom_flag/with_trap_exit_flag/with_true_value_then_false_value_exits_when_linked_process_does_not_exit_normal/init.erl
@@ -16,26 +16,29 @@ start() ->
     display(process_info(self(), trap_exit)),
     process_flag(trap_exit, false),
     display(process_info(self(), trap_exit)),
-    wait_to_shutdown(),
+    wait_to_shutdown_child(),
     shutdown(ChildPid),
     wait_to_shutdown()
-end),
+  end),
   receive
     {child_pid, ChildPid} ->
       ChildMonitorReference = monitor(process, ChildPid),
-      shutdown(ParentPid),
-      receive
-        {'DOWN', ParentMonitorReference, process, _, Reason1} ->
-          display({parent, exited, Reason1})
-      after 10 ->
-        display({parent, alive, is_process_alive(ParentPid)})
-      end,
+      shutdown_child(ParentPid),
       receive
         {'DOWN', ChildMonitorReference, process, _, Reason2} ->
           display({child, exited, Reason2})
       after 10 ->
         display({child, alive, is_process_alive(ChildPid)})
-      end
+      end,
+      receive
+        {'DOWN', ParentMonitorReference, process, _, Reason1} ->
+          display({parent, exited, Reason1})
+        after 10 ->
+          display({parent, alive, is_process_alive(ParentPid)})
+      end,
+      shutdown(ParentPid)
+    after 10 ->
+      display({parent, did, 'not', send, child_pid})
   end.
 
 shutdown(Pid) ->
@@ -44,4 +47,12 @@ shutdown(Pid) ->
 wait_to_shutdown() ->
   receive
     shutdown -> ok
+  end.
+
+shutdown_child(Pid) ->
+  Pid ! shutdown_child.
+
+wait_to_shutdown_child() ->
+  receive
+    shutdown_child -> ok
   end.

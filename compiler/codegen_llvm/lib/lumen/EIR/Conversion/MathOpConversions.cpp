a4309c24f31dea1a8e4ecf076bd12ea706f16ee0
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2020-09-10 00:16:34 -0400

    fix: constant folds of arithmetic ops

--- compiler/codegen_llvm/lib/lumen/EIR/Conversion/MathOpConversions.cpp
+++ compiler/codegen_llvm/lib/lumen/EIR/Conversion/MathOpConversions.cpp
@@ -2,14 +2,79 @@
 
 namespace lumen {
 namespace eir {
-template <typename Op, typename T>
-static Value specializeIntegerMathOp(Location loc,
-                                     RewritePatternContext<Op> &ctx, Value lhs,
-                                     Value rhs) {
-  Value lhsInt = ctx.decodeImmediate(lhs);
-  Value rhsInt = ctx.decodeImmediate(rhs);
-  auto mathOp = ctx.rewriter.template create<T>(loc, lhsInt, rhsInt);
-  return mathOp.getResult();
+
+template <typename Op>
+static void buildDeoptimizationPath(
+  Location loc,
+  RewritePatternContext<Op> &ctx,
+  Op op,
+  OpaqueTermType concreteTy,
+  StringRef intrinsicFn,
+  StringRef runtimeFn,
+  Value lhs, Value rhs) {
+
+  Operation *rawOp = op.getOperation();
+  Block *current = rawOp->getBlock();
+
+  Value lhsRaw = ctx.decodeImmediate(lhs);
+  Value rhsRaw = ctx.decodeImmediate(rhs);
+
+  // Build math op with overflow/underflow intrinsic
+  auto callee = ctx.rewriter.getSymbolRefAttr(intrinsicFn);
+  auto i1Ty = ctx.getI1Type();
+  auto termTy = ctx.getUsizeType();
+  auto i64Ty = ctx.getI64Type();
+  // TODO: Need to handle 32-bit or non-nanboxed 64 arches here
+  auto iFixTy = LLVMType::getIntNTy(ctx.rewriter.getContext(), 46);
+  auto resTy = LLVMType::getStructTy(ctx.rewriter.getContext(),
+                                     ArrayRef<LLVMType>{iFixTy, i1Ty},
+                                     /*packed=*/false);
+  auto calleeFn = ctx.getOrInsertFunction(intrinsicFn, resTy, {iFixTy, iFixTy});
+  Value lhsTrunc = llvm_trunc(iFixTy, lhsRaw);
+  Value rhsTrunc = llvm_trunc(iFixTy, rhsRaw);
+  Operation *callOp = llvm_call(ArrayRef<Type>{resTy}, callee, ArrayRef<Value>{lhsTrunc, rhsTrunc});
+
+  // Extract results
+  Value results = callOp->getResult(0);
+  Value resultFix = llvm_extractvalue(iFixTy, results, ctx.getI64ArrayAttr(0));
+  Value obit = llvm_extractvalue(i1Ty, results, ctx.getI64ArrayAttr(1));
+
+  // Split block after the above op
+  auto it = llvm::iplist<Operation>::iterator(obit.getDefiningOp());
+  auto splitStart = &*std::next(it);
+  Block *cont = current->splitBlock(splitStart);
+  cont->addArgument(termTy);
+
+  // Create overflow/normal blocks
+  Block *overflow = new Block();
+  Block *normal = new Block();
+  auto nextIt = std::next(Region::iterator(current));
+  current->getParent()->getBlocks().insert(nextIt, overflow);
+  current->getParent()->getBlocks().insert(nextIt, normal);
+
+  // Based on whether there was overflow/underflow,
+  // either branch to the deoptimized path (i.e. call
+  // to runtime function) or branch to the intermediate
+  // block where we re-encode the result and continue
+  // execution where we left off
+  ctx.rewriter.setInsertionPointToEnd(current);
+  llvm_condbr(obit, cont, ValueRange(), overflow, ValueRange());
+
+  // Handle normal
+  ctx.rewriter.setInsertionPointToEnd(normal);
+  Value extended = llvm_zext(termTy, resultFix);
+  Value encoded = ctx.encodeImmediate(concreteTy, extended);
+  llvm_br(ValueRange(encoded), cont);
+
+  // Handle overflow
+  ctx.rewriter.setInsertionPointToStart(overflow);
+  auto rtCallee = ctx.rewriter.getSymbolRefAttr(runtimeFn);
+  Operation *rtCallOp = llvm_call(ArrayRef<Type>{termTy}, rtCallee, ArrayRef<Value>{lhs, rhs});
+  llvm_br(rtCallOp->getResults(), cont);
+
+  // Replace original op with the final value
+  Value output = cont->getArgument(0);
+  ctx.rewriter.replaceOp(op, {output});
 }
 
 template <typename Op, typename T>
@@ -22,23 +87,55 @@ static Value specializeFloatMathOp(Location loc, RewritePatternContext<Op> &ctx,
   return fpOp.getResult();
 }
 
-template <typename Op, typename T>
-static Value specializeUnaryFloatMathOp(Location loc,
-                                        RewritePatternContext<Op> &ctx,
-                                        Value rhs) {
-  auto fpTy = ctx.getDoubleType();
-  Value r = eir_cast(rhs, fpTy);
-  auto fpOp = ctx.rewriter.template create<T>(loc, r);
-  return fpOp.getResult();
-}
+template <typename Op, typename OperandAdaptor>
+class SpecializedMathOpConversion : public EIROpConversion<Op> {
+public:
+  explicit SpecializedMathOpConversion(MLIRContext *context, EirTypeConverter &converter_,
+                            TargetInfo &targetInfo_,
+                            mlir::PatternBenefit benefit = 1)
+      : EIROpConversion<Op>::EIROpConversion(context, converter_, targetInfo_,
+                                             benefit) {}
 
-template <typename Op, typename T>
-static Value specializeLogicalOp(Location loc, RewritePatternContext<Op> &ctx,
-                                 Value lhs, Value rhs) {
-}
+  LogicalResult matchAndRewrite(
+      Op op, ArrayRef<Value> operands,
+      ConversionPatternRewriter &rewriter) const override {
+    auto loc = op.getLoc();
+    OperandAdaptor adaptor(operands);
+    auto ctx = getRewriteContext(op, rewriter);
 
-template <typename Op, typename OperandAdaptor, typename IntOp,
-          typename FloatOp>
+    Value lhs = adaptor.lhs();
+    Value rhs = adaptor.rhs();
+    Type lhsTy = op.getOperand(0).getType();
+    Type rhsTy = op.getOperand(1).getType();
+    StringRef intrinsic = Op::intrinsicSymbol();
+    StringRef builtinSymbol = Op::builtinSymbol();
+
+    // Use specialized lowerings if types are compatible
+    if (lhsTy.isa<FixnumType>() && rhsTy.isa<FixnumType>()) {
+      auto fixTy = rewriter.getType<FixnumType>();
+      buildDeoptimizationPath(loc, ctx, op, fixTy, intrinsic, builtinSymbol, lhs, rhs);
+      return success();
+    }
+
+    // Call builtin function
+    auto termTy = ctx.getUsizeType();
+    auto callee =
+        ctx.getOrInsertFunction(builtinSymbol, termTy, {termTy, termTy});
+
+    ArrayRef<Value> args({lhs, rhs});
+    auto calleeSymbol =
+        FlatSymbolRefAttr::get(builtinSymbol, callee->getContext());
+
+    rewriter.replaceOpWithNewOp<mlir::CallOp>(op, calleeSymbol,
+                                              ArrayRef<Type>{termTy}, args);
+    return success();
+  }
+
+ private:
+  using EIROpConversion<Op>::getRewriteContext;
+};
+
+template <typename Op, typename OperandAdaptor>
 class MathOpConversion : public EIROpConversion<Op> {
  public:
   explicit MathOpConversion(MLIRContext *context, EirTypeConverter &converter_,
@@ -56,20 +153,6 @@ class MathOpConversion : public EIROpConversion<Op> {
 
     Value lhs = adaptor.lhs();
     Value rhs = adaptor.rhs();
-    Type lhsTy = op.getOperand(0).getType();
-    Type rhsTy = op.getOperand(1).getType();
-
-    // Use specialized lowerings if types are compatible
-    if (lhsTy.isa<FixnumType>() && rhsTy.isa<FixnumType>()) {
-      auto newOp = specializeIntegerMathOp<Op, IntOp>(loc, ctx, lhs, rhs);
-      rewriter.replaceOp(op, newOp);
-      return success();
-    }
-    if (lhsTy.isa<FloatType>() && rhsTy.isa<FloatType>()) {
-      auto newOp = specializeFloatMathOp<Op, FloatOp>(loc, ctx, lhs, rhs);
-      rewriter.replaceOp(op, newOp);
-      return success();
-    }
 
     // Call builtin function
     StringRef builtinSymbol = Op::builtinSymbol();
@@ -103,20 +186,6 @@ struct NegOpConversion : public EIROpConversion<NegOp> {
     Value rhs = adaptor.rhs();
     Type rhsTy = rhs.getType();
 
-    // Use specialized lowerings if types are compatible
-    if (rhsTy.isa<FixnumType>()) {
-      Value rhsInt = ctx.decodeImmediate(rhs);
-      Value lhsInt = llvm_constant(rhsInt.getType(), ctx.getI32Attr(0));
-      rewriter.replaceOpWithNewOp<LLVM::SubOp>(op, lhsInt, rhsInt);
-      return success();
-    }
-    if (rhsTy.isa<FloatType>()) {
-      auto newOp =
-          specializeUnaryFloatMathOp<NegOp, LLVM::FNegOp>(loc, ctx, rhs);
-      rewriter.replaceOp(op, newOp);
-      return success();
-    }
-
     // Call builtin function
     StringRef builtinSymbol("erlang:-/1");
     auto termTy = ctx.getUsizeType();
@@ -133,19 +202,19 @@ struct NegOpConversion : public EIROpConversion<NegOp> {
 };
 
 struct AddOpConversion
-    : public MathOpConversion<AddOp, AddOpAdaptor, LLVM::AddOp, LLVM::FAddOp> {
-  using MathOpConversion::MathOpConversion;
+    : public SpecializedMathOpConversion<AddOp, AddOpAdaptor> {
+  using SpecializedMathOpConversion::SpecializedMathOpConversion;
 };
 struct SubOpConversion
-    : public MathOpConversion<SubOp, SubOpAdaptor, LLVM::SubOp, LLVM::FSubOp> {
-  using MathOpConversion::MathOpConversion;
+    : public SpecializedMathOpConversion<SubOp, SubOpAdaptor> {
+  using SpecializedMathOpConversion::SpecializedMathOpConversion;
 };
 struct MulOpConversion
-    : public MathOpConversion<MulOp, MulOpAdaptor, LLVM::MulOp, LLVM::FMulOp> {
-  using MathOpConversion::MathOpConversion;
+    : public SpecializedMathOpConversion<MulOp, MulOpAdaptor> {
+  using SpecializedMathOpConversion::SpecializedMathOpConversion;
 };
 
-template <typename Op, typename OperandAdaptor, typename IntOp>
+template <typename Op, typename OperandAdaptor>
 class IntegerMathOpConversion : public EIROpConversion<Op> {
  public:
   explicit IntegerMathOpConversion(MLIRContext *context,
@@ -167,13 +236,6 @@ class IntegerMathOpConversion : public EIROpConversion<Op> {
     Type lhsTy = op.getOperand(0).getType();
     Type rhsTy = op.getOperand(1).getType();
 
-    // Use specialized lowerings if types are compatible
-    if (lhsTy.isa<FixnumType>() && rhsTy.isa<FixnumType>()) {
-      auto newOp = specializeIntegerMathOp<Op, IntOp>(loc, ctx, lhs, rhs);
-      rewriter.replaceOp(op, newOp);
-      return success();
-    }
-
     // Call builtin function
     StringRef builtinSymbol = Op::builtinSymbol();
     auto termTy = ctx.getUsizeType();
@@ -192,32 +254,39 @@ class IntegerMathOpConversion : public EIROpConversion<Op> {
   using EIROpConversion<Op>::getRewriteContext;
 };
 
+// llvm.sdiv
 struct DivOpConversion
-    : public IntegerMathOpConversion<DivOp, DivOpAdaptor, LLVM::SDivOp> {
+    : public IntegerMathOpConversion<DivOp, DivOpAdaptor> {
   using IntegerMathOpConversion::IntegerMathOpConversion;
 };
+// llvm.srem
 struct RemOpConversion
-    : public IntegerMathOpConversion<RemOp, RemOpAdaptor, LLVM::SRemOp> {
+    : public IntegerMathOpConversion<RemOp, RemOpAdaptor> {
   using IntegerMathOpConversion::IntegerMathOpConversion;
 };
+// llvm.and
 struct BandOpConversion
-    : public IntegerMathOpConversion<BandOp, BandOpAdaptor, LLVM::AndOp> {
+    : public IntegerMathOpConversion<BandOp, BandOpAdaptor> {
   using IntegerMathOpConversion::IntegerMathOpConversion;
 };
+// llvm.or
 struct BorOpConversion
-    : public IntegerMathOpConversion<BorOp, BorOpAdaptor, LLVM::OrOp> {
+    : public IntegerMathOpConversion<BorOp, BorOpAdaptor> {
   using IntegerMathOpConversion::IntegerMathOpConversion;
 };
+// llvm.xor
 struct BxorOpConversion
-    : public IntegerMathOpConversion<BxorOp, BxorOpAdaptor, LLVM::XOrOp> {
+    : public IntegerMathOpConversion<BxorOp, BxorOpAdaptor> {
   using IntegerMathOpConversion::IntegerMathOpConversion;
 };
+// llvm.shl
 struct BslOpConversion
-    : public IntegerMathOpConversion<BslOp, BslOpAdaptor, LLVM::ShlOp> {
+    : public IntegerMathOpConversion<BslOp, BslOpAdaptor> {
   using IntegerMathOpConversion::IntegerMathOpConversion;
 };
+// llvm.ashr
 struct BsrOpConversion
-    : public IntegerMathOpConversion<BsrOp, BsrOpAdaptor, LLVM::AShrOp> {
+    : public IntegerMathOpConversion<BsrOp, BsrOpAdaptor> {
   using IntegerMathOpConversion::IntegerMathOpConversion;
 };
 
@@ -245,8 +314,8 @@ class FloatMathOpConversion : public EIROpConversion<Op> {
 
     // Use specialized lowerings if types are compatible
     if (lhsTy.isa<FloatType>() && rhsTy.isa<FloatType>()) {
-      auto newOp = specializeIntegerMathOp<Op, FloatOp>(loc, ctx, lhs, rhs);
-      rewriter.replaceOp(op, newOp);
+      auto result = specializeFloatMathOp<Op, FloatOp>(loc, ctx, lhs, rhs);
+      rewriter.replaceOp(op, result);
       return success();
     }
 

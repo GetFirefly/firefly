91d2cf187d1f7f6e6ad75798de6888c5ddf3a566
Luke Imhoff <Kronic.Deth@gmail.com>
2020-09-15 11:13:58 -0500

    Integration tests for andalso/2

--- native_implemented/otp/tests/lib/erlang/andalso_2.rs
+++ native_implemented/otp/tests/lib/erlang/andalso_2.rs
@@ -1,4 +1,3 @@
-// `without_boolean_left_errors_badarg` in unit tests
-
+test_stdout!(without_boolean_left_errors_badarg, "{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n");
 test_stdout!(with_false_left_returns_false, "false\n");
 test_stdout!(with_true_left_returns_right, "right\n");

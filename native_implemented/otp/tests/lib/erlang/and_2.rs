1b5cd93b5bac378f640770a96e681141a4f366f6
Luke Imhoff <Kronic.Deth@gmail.com>
2020-09-15 10:59:44 -0500

    Integration tests for and/2

--- native_implemented/otp/tests/lib/erlang/and_2.rs
+++ native_implemented/otp/tests/lib/erlang/and_2.rs
@@ -3,4 +3,4 @@ mod with_false_left;
 #[path = "and_2/with_true_left.rs"]
 mod with_true_left;
 
-// `without_boolean_left_errors_badarg` in unit tests
+test_stdout!(without_boolean_left_errors_badarg, "{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n");
--- native_implemented/otp/tests/lib/erlang/and_2/with_true_left.rs
+++ native_implemented/otp/tests/lib/erlang/and_2/with_true_left.rs
@@ -1,4 +1,3 @@
-// `without_boolean_right_errors_badarg` in unit tests
-
+test_stdout!(without_boolean_right_errors_badarg, "{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n");
 test_stdout!(with_false_right_returns_false, "false\n");
 test_stdout!(with_true_right_returns_true, "true\n");

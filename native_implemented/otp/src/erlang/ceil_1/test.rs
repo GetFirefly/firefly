215017b27b3e4d911af9628f44c6b3bf04c276d6
Luke Imhoff <Kronic.Deth@gmail.com>
2020-09-29 09:41:43 -0500

    Integration tests for erlang:ceil/1 including badarg

--- native_implemented/otp/src/erlang/ceil_1/test.rs
+++
@@ -1,9 +0,0 @@
-use crate::erlang::ceil_1::result;
-
-#[test]
-fn without_number_errors_badarg() {
-    crate::test::without_number_errors_badarg(file!(), result);
-}
-
-// `with_integer_returns_integer` in integration tests
-// `with_float_round_up_to_next_integer` in integration tests
--- native_implemented/otp/tests/internal/lib/erlang/ceil_1.rs
+++ native_implemented/otp/tests/internal/lib/erlang/ceil_1.rs
@@ -1,4 +1,3 @@
-// `without_number_errors_badarg` in unit tests
-
+test_stdout!(without_number_errors_badarg, "{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n");
 test_stdout!(with_integer_returns_integer, "-1\n0\n1\n");
 test_stdout!(with_float_round_up_to_next_integer, "-1\n-1\n0\n1\n1\n");

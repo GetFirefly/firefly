d2695e383a12a02140970a72c19f438ae5384fd0
Luke Imhoff <Kronic.Deth@gmail.com>
2020-09-29 12:52:14 -0500

    Integration tests for convert_time_unit/3 including badarg

---
+++ native_implemented/otp/tests/internal/lib/erlang/convert_time_unit_3/with_integer_time_with_unit_from_unit_without_unit_to_unit_errors_badarg/init.erl
@@ -0,0 +1,15 @@
+-module(init).
+-export([start/0]).
+-import(erlang, [convert_time_unit/3]).
+
+start() ->
+  test:each(fun
+    (Integer) when is_integer(Integer) -> ignore;
+    (Atom) when is_atom(Atom) -> ignore;
+    (Term) -> test(Term)
+  end).
+
+test(ToUnit) ->
+  test:caught(fun () ->
+    convert_time_unit(0, native, ToUnit)
+  end).

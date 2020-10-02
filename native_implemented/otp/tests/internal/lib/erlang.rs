50a62b70403e8c383171dd633916ad6d7a2a9d38
Luke Imhoff <Kronic.Deth@gmail.com>
2020-10-01 19:14:53 -0500

    Integration tests for is_pid/1

--- native_implemented/otp/tests/internal/lib/erlang.rs
+++ native_implemented/otp/tests/internal/lib/erlang.rs
@@ -126,6 +126,8 @@ pub mod is_list_1;
 pub mod is_map_1;
 #[path = "erlang/is_number_1.rs"]
 pub mod is_number_1;
+#[path = "erlang/is_pid_1.rs"]
+pub mod is_pid_1;
 #[path = "erlang/link_1.rs"]
 pub mod link_1;
 #[path = "erlang/or_2.rs"]

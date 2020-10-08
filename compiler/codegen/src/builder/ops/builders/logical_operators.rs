90b5db7aa881c21083639901d3698d5ffe5e380f
Luke Imhoff <Kronic.Deth@gmail.com>
2020-10-08 13:04:02 -0500

    Support variadic logic and and or from EIR
    LogicalOperator has a Vec of operands now instead of just rhs and lhs.  The operands vec is converted to a vec of ValueRefs and passed to teh MLIRBuildLogic*Op C functions as a ptr and len like the argc and argc.  When converting to MLIR Operations though, the variadics are removed and instead a chain of LogicalAndOp or LogicalOrOp are constructed, so that the operation can continue to subclass BinaryOperator.

--- compiler/codegen/src/builder/ops/builders/logical_operators.rs
+++ compiler/codegen/src/builder/ops/builders/logical_operators.rs
@@ -11,12 +11,35 @@ impl LogicOpBuilder {
         op: LogicalOperator,
     ) -> Result<Option<Value>> {
         let builder_ref = builder.as_ref();
-        let lhs_ref = builder.value_ref(op.lhs);
-        let rhs_ref = op.rhs.map(|v| builder.value_ref(v)).unwrap_or_default();
+        let operand_refs = op
+            .operands
+            .iter()
+            .copied()
+            .map(|value| builder.value_ref(value))
+            .collect::<Vec<_>>();
+        builder.debug(&format!(
+            "logic operation ({:?}) operands: {:?}",
+            op.kind,
+            operand_refs.as_slice()
+        ));
 
         let result_ref = match op.kind {
-            LogicOp::And => unsafe { MLIRBuildLogicalAndOp(builder_ref, op.loc, lhs_ref, rhs_ref) },
-            LogicOp::Or => unsafe { MLIRBuildLogicalOrOp(builder_ref, op.loc, lhs_ref, rhs_ref) },
+            LogicOp::And => unsafe {
+                MLIRBuildLogicalAndOp(
+                    builder_ref,
+                    op.loc,
+                    operand_refs.as_ptr(),
+                    operand_refs.len() as libc::c_uint,
+                )
+            },
+            LogicOp::Or => unsafe {
+                MLIRBuildLogicalOrOp(
+                    builder_ref,
+                    op.loc,
+                    operand_refs.as_ptr(),
+                    operand_refs.len() as libc::c_uint,
+                )
+            },
             LogicOp::Eq => todo!("logical primop (eq)"),
         };
         assert!(!result_ref.is_null());

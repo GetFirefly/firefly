6208afedd89b779c5737843fef4d6ecdadd2a720
Luke Imhoff <Kronic.Deth@gmail.com>
2020-10-12 20:27:05 -0500

    erlang:seq_trace_print/1
    Stub to always return `false` as turning on seq_trace isn't actually supported yet

---
+++ native_implemented/otp/src/erlang/seq_trace_print_1.rs
@@ -0,0 +1,9 @@
+use liblumen_alloc::erts::term::prelude::*;
+
+// See https://github.com/lumen/otp/blob/30e2bfb9f1fd5c65bd7d9a4159f88cdcf72023fa/erts/emulator/beam/erl_bif_trace.c#L1919-L1936
+#[native_implemented::function(erlang:seq_trace_print/1)]
+pub fn result(_message: Term) -> Term {
+    // Stub as if `tracing_token` is not set
+    // See https://github.com/lumen/otp/blob/30e2bfb9f1fd5c65bd7d9a4159f88cdcf72023fa/erts/emulator/beam/erl_bif_trace.c#L1930
+    false.into()
+}

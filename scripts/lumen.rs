13d833bfeccf74cc122379a25a237f272d5c5669
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2020-10-27 10:47:52 -0400

    build: do not attach stdin to internal build command

--- scripts/lumen.rs
+++ scripts/lumen.rs
@@ -172,6 +172,7 @@ fn main() {
 
     let cmd = format!("{:?}", &cargo_cmd);
     let mut child = cargo_cmd
+        .stdin(Stdio::null())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()

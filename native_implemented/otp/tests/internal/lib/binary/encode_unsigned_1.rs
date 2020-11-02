112820f08118a0d357b3a6afd774762498307eb4
Mike Wilkerson <mwilkerson@gmail.com>
2020-11-02 13:20:56 -0800

    migrate more integration tests

--- native_implemented/otp/tests/internal/lib/binary/encode_unsigned_1.rs
+++ native_implemented/otp/tests/internal/lib/binary/encode_unsigned_1.rs
@@ -1,2 +1,4 @@
 test_stdout!(doctest, "<<169,138,199>>\n");
 test_stdout!(with_smallest_big_int, "<<64,0,0,0,0,0>>\n");
+test_stdout!(with_non_integer, "{caught, error, badarg}\n{caught, error, badarg}\n{caught, error, badarg}\n");
+test_stdout!(with_negative_integer, "{caught, error, badarg}\n{caught, error, badarg}\n");

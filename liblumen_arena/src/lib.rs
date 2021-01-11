6bf926fd02fbcf91960f55af16cd8a6d6d44a716
Luke Imhoff <Kronic.Deth@gmail.com>
2021-01-10 21:17:16 -0600

    Update liblumen_arena
    Port changes from upstream as of https://github.com/rust-lang/rust/blob/26d451f4b3bcbea68eb80856aeddf37af52991ac/compiler/rustc_arena/src/lib.rs.  Did not include `from_iter` APIs as it appears we don't need them.

--- liblumen_arena/src/lib.rs
+++ liblumen_arena/src/lib.rs
@@ -4,6 +4,8 @@
 #![feature(dropck_eyepatch)]
 // Used for arith_offset
 #![feature(core_intrinsics)]
+#![feature(new_uninit)]
+#![feature(maybe_uninit_slice)]
 // Used for the implementation of the arenas
 #![feature(raw_vec_internals)]
 

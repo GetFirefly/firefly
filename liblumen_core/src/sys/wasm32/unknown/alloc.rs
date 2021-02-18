95b0c28fd80b5e4118d114fd466589739a5213d0
Luke Imhoff <Kronic.Deth@gmail.com>
2021-02-17 22:26:30 -0600

    Fix wasm32 allocator
    * Update dlmalloc to 0.2.1.
    * Switch `alloc` functions to match `Allocator` functions names as was done for `unix`.
    * Switch `mmap` fallbacks to use new `Allocator` names.

--- liblumen_core/src/sys/wasm32/unknown/alloc.rs
+++ liblumen_core/src/sys/wasm32/unknown/alloc.rs
@@ -1,73 +1,87 @@
-use core::alloc::{AllocError, Layout, MemoryBlock};
+use core::alloc::{AllocError, Layout};
+use core::ptr::{self, NonNull};
 
-use core::ptr::NonNull;
+use dlmalloc::Dlmalloc;
 
 use crate::locks::SpinLock;
 
-static SYS_ALLOC_LOCK: SpinLock<dlmalloc::Dlmalloc> = SpinLock::new(dlmalloc::DLMALLOC_INIT);
+static SYS_ALLOC_LOCK: SpinLock<Dlmalloc> = SpinLock::new(Dlmalloc::new());
 
 #[inline]
-pub fn alloc(layout: Layout) -> Result<MemoryBlock, AllocError> {
+pub fn allocate(layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
     let mut allocator = SYS_ALLOC_LOCK.lock();
     let layout_size = layout.size();
     let ptr = unsafe { (*allocator).malloc(layout_size, layout.align()) };
-    NonNull::new(ptr).ok_or(AllocError).map(|ptr| MemoryBlock {
-        ptr,
-        size: layout_size,
-    })
+    NonNull::new(ptr)
+        .ok_or(AllocError)
+        .map(|ptr| NonNull::slice_from_raw_parts(ptr, layout_size))
 }
 
 #[inline]
-pub fn alloc_zeroed(layout: Layout) -> Result<MemoryBlock, AllocError> {
+pub fn allocate_zeroed(layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
     let mut allocator = SYS_ALLOC_LOCK.lock();
     let layout_size = layout.size();
     let ptr = unsafe { (*allocator).calloc(layout_size, layout.align()) };
-    NonNull::new(ptr).ok_or(AllocError).map(|ptr| MemoryBlock {
-        ptr,
-        size: layout_size,
-    })
+    NonNull::new(ptr)
+        .ok_or(AllocError)
+        .map(|ptr| NonNull::slice_from_raw_parts(ptr, layout_size))
+}
+
+#[inline]
+pub unsafe fn deallocate(ptr: NonNull<u8>, layout: Layout) {
+    let mut allocator = SYS_ALLOC_LOCK.lock();
+    (*allocator).free(ptr.as_ptr(), layout.size(), layout.align());
 }
 
 #[inline]
 pub unsafe fn grow(
-    ptr: *mut u8,
-    layout: Layout,
-    new_size: usize,
-) -> Result<MemoryBlock, AllocError> {
-    let old_size = layout.size();
-    let block = self::realloc(ptr, layout, new_size)?;
-    AllocInit::init_offset(init, block, old_size);
-    Ok(block)
+    ptr: NonNull<u8>,
+    old_layout: Layout,
+    new_layout: Layout,
+) -> Result<NonNull<[u8]>, AllocError> {
+    reallocate(ptr, old_layout, new_layout)
+}
+
+#[inline]
+pub unsafe fn grow_zeroed(
+    ptr: NonNull<u8>,
+    old_layout: Layout,
+    new_layout: Layout,
+) -> Result<NonNull<[u8]>, AllocError> {
+    reallocate(ptr, old_layout, new_layout).map(|ptr| {
+        let old_size = old_layout.size();
+        let dst = ptr.as_mut_ptr().add(old_size);
+        let count = new_layout.size() - old_size;
+        ptr::write_bytes(dst, 0, count);
+
+        ptr
+    })
 }
 
 #[inline]
 pub unsafe fn shrink(
-    ptr: *mut u8,
-    layout: Layout,
-    new_size: usize,
-) -> Result<MemoryBlock, AllocError> {
-    self::realloc(ptr, layout, new_size, placement)
+    ptr: NonNull<u8>,
+    old_layout: Layout,
+    new_layout: Layout,
+) -> Result<NonNull<[u8]>, AllocError> {
+    reallocate(ptr, old_layout, new_layout)
 }
 
 #[inline]
-pub unsafe fn realloc(
-    ptr: *mut u8,
-    layout: Layout,
-    new_size: usize,
-) -> Result<MemoryBlock, AllocError> {
+unsafe fn reallocate(
+    ptr: NonNull<u8>,
+    old_layout: Layout,
+    new_layout: Layout,
+) -> Result<NonNull<[u8]>, AllocError> {
+    // TODO handle changed align
+    assert!(old_layout.align() == new_layout.align());
     let mut allocator = SYS_ALLOC_LOCK.lock();
-    let layout_size = layout.size();
-    let new_ptr = (*allocator).realloc(ptr, layout_size, layout.align(), new_size);
+    let old_size = old_layout.size();
+    let old_align = old_layout.align();
+    let new_size = new_layout.size();
+    let new_ptr = (*allocator).realloc(ptr.as_ptr(), old_size, old_align, new_size);
     NonNull::new(new_ptr)
         .ok_or(AllocError)
-        .map(|ptr| MemoryBlock {
-            ptr,
-            size: layout_size,
-        })
+        .map(|ptr| NonNull::slice_from_raw_parts(ptr, new_size))
 }
 
-#[inline]
-pub unsafe fn free(ptr: *mut u8, layout: Layout) {
-    let mut allocator = SYS_ALLOC_LOCK.lock();
-    (*allocator).free(ptr, layout.size(), layout.align());
-}

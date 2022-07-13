3f10831cdac59edf19311ad85a64f2d704a5e30b
Elle Imhoff <Kronic.Deth@gmail.com>
2022-07-13 14:08:17 -0500

    Add macOS x86_64 atom and symbol start and end functions

--- runtimes/crt/src/atoms.rs
+++ runtimes/crt/src/atoms.rs
@@ -8,6 +8,8 @@ extern "C-unwind" {
     pub fn init(start: *const AtomData, end: *const AtomData) -> bool;
 }
 
+
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 pub(super) fn start() -> *const AtomData {
     let mut ptr: *mut AtomData = core::ptr::null_mut();
     unsafe {
@@ -21,12 +23,39 @@ pub(super) fn start() -> *const AtomData {
     ptr
 }
 
+#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+pub(super) fn start() -> *const AtomData {
+    let mut ptr: *mut AtomData = core::ptr::null_mut();
+    unsafe {
+        asm!(
+            "mov {x}, [rip + section$start$__DATA$__atoms@GOTPCREL]",
+            x = inout(reg) ptr,
+            options(readonly, preserves_flags, nostack)
+        );
+    }
+    ptr
+}
+
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 pub(super) fn end() -> *const AtomData {
     let mut ptr: *mut AtomData = core::ptr::null_mut();
     unsafe {
         asm!(
             "adrp {x}, section$end$__DATA$__atoms@GOTPAGE",
             "ldr {x}, [{x}, section$end$__DATA$__atoms@GOTPAGEOFF]",
+            x = inout(reg) ptr,
+            options(readonly, preserves_flags, nostack)
+        );
+    }
+    ptr
+}
+
+#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+pub(super) fn end() -> *const AtomData {
+    let mut ptr: *mut AtomData = core::ptr::null_mut();
+    unsafe {
+        asm!(
+            "mov {x}, [rip + section$end$__DATA$__atoms@GOTPCREL]",
             x = inout(reg) ptr,
             options(readonly, preserves_flags, nostack)
         );
--- runtimes/crt/src/symbols.rs
+++ runtimes/crt/src/symbols.rs
@@ -7,6 +7,7 @@ extern "C-unwind" {
     pub fn init(start: *const FunctionSymbol, end: *const FunctionSymbol) -> bool;
 }
 
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 pub(super) fn start() -> *const FunctionSymbol {
     let mut ptr: *mut FunctionSymbol = core::ptr::null_mut();
     unsafe {
@@ -20,12 +21,39 @@ pub(super) fn start() -> *const FunctionSymbol {
     ptr
 }
 
+#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+pub(super) fn start() -> *const FunctionSymbol {
+    let mut ptr: *mut FunctionSymbol = core::ptr::null_mut();
+    unsafe {
+        asm!(
+        "mov {x}, [rip + section$start$__DATA$__dispatch@GOTPCREL]",
+        x = inout(reg) ptr,
+        options(readonly, preserves_flags, nostack)
+        );
+    }
+    ptr
+}
+
+#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
 pub(super) fn end() -> *const FunctionSymbol {
     let mut ptr: *mut FunctionSymbol = core::ptr::null_mut();
     unsafe {
         asm!(
             "adrp {x}, section$end$__DATA$__dispatch@GOTPAGE",
             "ldr {x}, [{x}, section$end$__DATA$__dispatch@GOTPAGEOFF]",
+            x = inout(reg) ptr,
+            options(readonly, preserves_flags, nostack)
+        );
+    }
+    ptr
+}
+
+#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+pub(super) fn end() -> *const FunctionSymbol {
+    let mut ptr: *mut FunctionSymbol = core::ptr::null_mut();
+    unsafe {
+        asm!(
+            "mov {x}, [rip + section$end$__DATA$__dispatch@GOTPCREL]",
             x = inout(reg) ptr,
             options(readonly, preserves_flags, nostack)
         );
